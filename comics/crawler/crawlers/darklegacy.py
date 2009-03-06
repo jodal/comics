f45c4d7adfa40ddb9e9ee4986d16d9320b4b9f24
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-03-06 09:03:14 +0100

    Update Dark Legacy crawler

--- comics/crawler/crawlers/darklegacy.py
+++ comics/crawler/crawlers/darklegacy.py
@@ -4,9 +4,8 @@ class ComicCrawler(BaseComicCrawler):
     def _get_url(self):
         self.parse_feed('http://www.darklegacycomics.com/feed.xml')
 
-        for entry in self.feed['entries']:
-            pub_date = self.string_to_date(entry['summary'], 'Updated %m/%d/%y')
-            if pub_date == self.pub_date:
-                self.title = entry['title']
-                self.url = entry['link'].replace('.html', '.jpg')
+        for entry in self.feed.entries:
+            if self.timestamp_to_date(entry.updated_parsed) == self.pub_date:
+                self.title = entry.title
+                self.url = entry.link.replace('.html', '.jpg')
                 return
