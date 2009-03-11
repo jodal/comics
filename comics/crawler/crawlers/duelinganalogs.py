02265a09de20fb67dbfb3bfcae82788b5a893f6e
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-03-11 19:42:45 +0100

    Add crawler for Dueling Analogs

---
+++ comics/crawler/crawlers/duelinganalogs.py
@@ -0,0 +1,15 @@
+from comics.crawler.crawlers import BaseComicCrawler
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.parse_feed(
+            'http://feeds2.feedburner.com/DuelingAnalogs?format=xml')
+
+        for entry in self.feed['entries']:
+            if self.timestamp_to_date(entry['updated_parsed']) == self.pub_date:
+                self.title = entry['title']
+                pieces = entry['summary'].split('"')
+                for i, piece in enumerate(pieces):
+                    if piece.count('src='):
+                        self.url = pieces[i + 1]
+                        return
