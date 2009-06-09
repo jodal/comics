9ce2de8045c5b04efdac5b533b2d88dfcf7b5d54
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-06-09 13:41:21 +0200

    Fix asofterworld crawler

--- comics/crawler/crawlers/asofterworld.py
+++ comics/crawler/crawlers/asofterworld.py
@@ -6,7 +6,7 @@ class ComicCrawler(BaseComicCrawler):
 
         for entry in self.feed.entries:
             if (self.timestamp_to_date(entry.updated_parsed) == self.pub_date
-                and entry.link == 'http://www.asofterworld.com'):
+                and entry.link != 'http://www.asofterworld.com'):
                 self.title = entry.title
                 pieces = entry.summary.split('"')
                 for i, piece in enumerate(pieces):
