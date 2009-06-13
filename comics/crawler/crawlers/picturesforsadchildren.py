4556cc8e15c9207d937465586f5d6149f54405c2
Thomas Adamcik <adamcik@samfundet.no>
2009-06-13 03:22:12 +0200

    Add 'pictures for sad children' crawler

---
+++ comics/crawler/crawlers/picturesforsadchildren.py
@@ -0,0 +1,14 @@
+from comics.crawler.crawlers import BaseComicCrawler
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.parse_feed('http://www.rsspect.com/rss/pfsc.xml')
+
+        for entry in self.feed.entries:
+            if self.timestamp_to_date(entry.updated_parsed) == self.pub_date:
+                self.title = entry.title
+                pieces = entry.summary.split('"')
+                for i, piece in enumerate(pieces):
+                    if piece.count('src='):
+                        self.url = pieces[i + 1]
+                        return
