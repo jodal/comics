21d6dd1d44028bb1881a30cae140d7f85ff09d27
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-07-14 23:28:08 +0200

    Add loadcomicmeta command that does nothing

---
+++ comics/crawler/meta.py
@@ -0,0 +1,11 @@
+class ComicMetaLoader(object):
+    def __init__(self, options):
+        self.options = options
+        # TODO Use logic from SuperCrawler._get_comic_crawler() to get the
+        # crawler module, and thus the ComicMeta object.
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
