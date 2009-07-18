c306d4e056bf58987e6a8253c152b510c43e7bfe
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-07-18 13:47:24 +0200

    Implement most of ComicMetaLoader

--- comics/crawler/meta.py
+++ comics/crawler/meta.py
@@ -1,11 +1,42 @@
+import logging
+
+from django.conf import settings
+
+from comics.common.models import Comic
+from comics.crawler.utils import get_comic_module
+
+logger = logging.getLogger('comics.crawler.meta')
+
 class ComicMetaLoader(object):
     def __init__(self, options):
-        self.options = options
-        # TODO Use logic from SuperCrawler._get_comic_crawler() to get the
-        # crawler module, and thus the ComicMeta object.
+        self.comic_slugs = self._get_comic_slugs(options)
 
     def start(self):
-        pass
+        for comic_slug in self.comic_slugs:
+            logger.info('Loading meta data for %s', comic_slug)
+            self._try_load_comic_meta(comic_slug)
 
     def stop(self):
         pass
+
+    def _get_comic_slugs(self, options):
+        comic_slugs = options.get('comic_slugs', None)
+        if comic_slugs is None or len(comic_slugs) == 0:
+            logger.error('Loading all comics is not implemented yet, '
+                'use "-c COMIC".')
+            return [] # TODO Add all comic slugs
+        else:
+            return comic_slugs
+
+    def _try_load_comic_meta(self, comic_slug):
+        try:
+            self._load_comic_meta(comic_slug)
+        except Exception, error:
+            logger.exception(error)
+
+    def _load_comic_meta(self, comic_slug):
+        logger.debug('Importing comic module')
+        comic_module = get_comic_module(comic_slug)
+        comic_meta = comic_module.ComicMeta()
+        logger.debug('Loading comic into database')
+        comic_meta.create_comic()
