b70e155fba70d4a7c946b7d64de6d70c735c408d
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-08-03 23:17:21 +0200

    Fix typo in 'gws' comic URL

--- comics/crawler/crawlers/gws.py
+++ comics/crawler/crawlers/gws.py
@@ -4,7 +4,7 @@ from comics.crawler.meta import BaseComicMeta
 class ComicMeta(BaseComicMeta):
     name = 'Girls With Slingshots'
     language = 'en'
-    url = 'http://www.girlswithslingshot.com/'
+    url = 'http://www.girlswithslingshots.com/'
     start_date = '2004-09-30'
     history_capable_days = 1
     schedule = 'Mo,Tu,We,Th,Fr'
