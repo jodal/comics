05659cd132a5dfb54b50ec38ff1d405697de251a
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-09-06 21:52:14 +0200

    Add crawler for superpoop

---
+++ comics/crawler/crawlers/superpoop.py
@@ -0,0 +1,25 @@
+from comics.crawler.base import BaseComicCrawler
+from comics.crawler.meta import BaseComicMeta
+
+class ComicMeta(BaseComicMeta):
+    name = 'Superpoop'
+    language = 'en'
+    url = 'http://www.superpoop.com/'
+    start_date = '2008-01-01'
+    history_capable_days = 30
+    schedule = 'Mo,Tu,We,Th'
+    time_zone = -5
+    rights = 'Drew'
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.parse_feed('http://www.superpoop.com/rss/rss.php')
+
+        for entry in self.feed.entries:
+            if self.timestamp_to_date(entry.updated_parsed) == self.pub_date:
+                self.title = entry.title
+                pieces = entry.summary.split('"')
+                for i, piece in enumerate(pieces):
+                    if piece.count('src='):
+                        self.url = pieces[i + 1]
+                        return
