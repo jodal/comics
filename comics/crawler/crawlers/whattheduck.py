4f8df6926d81c8da37a4d28b932936062ff6847a
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-10-27 21:44:15 +0100

    whattheduck has a new feed which is not updated with titles

--- comics/crawler/crawlers/whattheduck.py
+++ comics/crawler/crawlers/whattheduck.py
@@ -12,9 +12,7 @@ class ComicMeta(BaseComicMeta):
 
 class ComicCrawler(BaseComicCrawler):
     def _get_url(self):
-        self.parse_feed(
-            'http://web.mac.com/aaronandpatty/'
-            'What_the_Duck/Comic_Strips/rss.xml')
+        self.parse_feed('http://www.whattheduck.net/strip/rss.xml')
 
         for entry in self.feed.entries:
             if (self.timestamp_to_date(entry.updated_parsed) == self.pub_date
@@ -23,23 +21,3 @@ class ComicCrawler(BaseComicCrawler):
                 self.title = entry.title
                 self.url = entry.enclosures[0].href
                 return
-
-    def update_titles(self):
-        self.parse_feed(
-            'http://web.mac.com/aaronandpatty/'
-            'What_the_Duck/Comic_Strips/rss.xml')
-
-        counter = 0
-        for entry in self.feed.entries:
-            split_title = entry.title.split(':')
-            if len(split_title) > 1:
-                try:
-                    strip = self.comic.strip_set.filter(
-                        title__startswith=split_title[0])[0]
-                    if strip.title != entry.title:
-                        strip.title = entry.title
-                        strip.save()
-                        counter += 1
-                except IndexError:
-                    pass
-        return counter
