74705a7571919a30658d6d9dd8fb289a79c01772
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-10-27 22:14:35 +0100

    Remove redundancy in URL config. Remove support for dates before year 1900 and after 2099 as datetime does not support them.

--- comics/core/urls.py
+++ comics/core/urls.py
@@ -2,35 +2,36 @@ from django.conf.urls.defaults import *
 
 from comics.core import views
 
+YEAR = r'(?P<year>(19|20)\d{2})'
+MONTH = r'(?P<month>\d{1,2})'
+DAY = r'(?P<day>\d{1,2})'
+DAYS = r'\+(?P<days>\d+)'
+COMIC = r'(?P<comic>[0-9a-z-_]+)'
+
 urlpatterns = patterns('',
     # View top comics
-    url(r'^$',
-        views.top_latest, name='top-latest'),
-    url(r'^\+(?P<days>\d+)/$',
-        views.top_show, name='top-last-days'),
-    url(r'^(?P<year>\d{4})/$',
-        views.top_year, name='top-year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
-        views.top_show, name='top-month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
-        views.top_show, name='top-date'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/\+(?P<days>\d+)/$',
+    url(r'^$', views.top_latest, name='top-latest'),
+    url(r'^%s/$' % (DAYS,), views.top_show, name='top-last-days'),
+    url(r'^%s/$' % (YEAR,), views.top_year, name='top-year'),
+    url(r'^%s/%s/$' % (YEAR, MONTH), views.top_show, name='top-month'),
+    url(r'^%s/%s/%s/$' % (YEAR, MONTH, DAY), views.top_show, name='top-date'),
+    url(r'^%s/%s/%s/%s/$' % (YEAR, MONTH, DAY, DAYS),
         views.top_show, name='top-date-days'),
 
     # View one specific comic
     url(r'^c/$',
         views.comic_list, name='comic-list'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/$',
+    url(r'^c/%s/$' % (COMIC,),
         views.comic_latest, name='comic-latest'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/\+(?P<days>\d+)/$',
+    url(r'^c/%s/%s/$' % (COMIC, DAYS),
         views.comic_show, name='comic-last-days'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/(?P<year>\d{4})/$',
+    url(r'^c/%s/%s/$' % (COMIC, YEAR),
         views.comic_year, name='comic-year'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+    url(r'^c/%s/%s/%s/$' % (COMIC, YEAR, MONTH),
         views.comic_show, name='comic-month'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
+    url(r'^c/%s/%s/%s/%s/$' % (COMIC, YEAR, MONTH, DAY),
         views.comic_show, name='comic-date'),
-    url(r'^c/(?P<comic>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/\+(?P<days>\d+)/$',
+    url(r'^c/%s/%s/%s/%s/%s/$' % (COMIC, YEAR, MONTH, DAY, DAYS),
         views.comic_show, name='comic-date-days'),
 
     # About page
--- comics/sets/urls.py
+++ comics/sets/urls.py
@@ -1,24 +1,27 @@
 from django.conf.urls.defaults import *
 
+from comics.core.urls import YEAR, MONTH, DAY, DAYS
 from comics.sets import views
+
+SET = r'(?P<set>[0-9a-z-_]+)'
 
 urlpatterns = patterns('',
     # Comic sets
     url(r'^$',
         views.set_new, name='set-new'),
-    url(r'^(?P<set>[0-9a-z-_]+)/edit/$',
+    url(r'^%s/edit/$' % (SET,),
         views.set_edit, name='set-edit'),
 
-    url(r'^(?P<set>[0-9a-z-_]+)/$',
+    url(r'^%s/$' % (SET,),
         views.set_latest, name='set-latest'),
-    url(r'^(?P<set>[0-9a-z-_]+)/\+(?P<days>\d+)/$',
+    url(r'^%s/%s/$' % (SET, DAYS),
         views.set_show, name='set-last-days'),
-    url(r'^(?P<set>[0-9a-z-_]+)/(?P<year>\d{4})/$',
+    url(r'^%s/%s/$' % (SET, YEAR),
         views.set_year, name='set-year'),
-    url(r'^(?P<set>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+    url(r'^%s/%s/%s/$' % (SET, YEAR, MONTH),
         views.set_show, name='set-month'),
-    url(r'^(?P<set>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
+    url(r'^%s/%s/%s/%s/$' % (SET, YEAR, MONTH, DAY),
         views.set_show, name='set-date'),
-    url(r'^(?P<set>[0-9a-z-_]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/\+(?P<days>\d+)/$',
+    url(r'^%s/%s/%s/%s/%s/$' % (SET, YEAR, MONTH, DAY, DAYS),
         views.set_show, name='set-date-days'),
 )
