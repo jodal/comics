5a21b66f7ab77f419245d8c07d7473a6e1600fc4
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-15 23:07:55 +0100

    Add crawler for 'Hark, A Vagrant'

---
+++ comics/crawler/crawlers/harkavagrant.py
@@ -0,0 +1,27 @@
+from comics.crawler.base import BaseComicCrawler
+from comics.crawler.meta import BaseComicMeta
+
+class ComicMeta(BaseComicMeta):
+    name = 'Hark, A Vagrant!'
+    language = 'en'
+    url = 'http://www.harkavagrant.com/'
+    start_date = '2008-05-01'
+    history_capable_days = 120
+    schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
+    time_zone = -8
+    rights = 'Kate Beaton'
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.parse_feed('http://www.rsspect.com/rss/vagrant.xml')
+
+        for entry in self.feed.entries:
+            if self.timestamp_to_date(entry.updated_parsed) == self.pub_date:
+                pieces = entry.summary.split('"')
+                for i, piece in enumerate(pieces):
+                    if piece.count('src='):
+                        self.url = pieces[i + 1]
+                    if piece.count('title='):
+                        self.title = pieces[i + 1]
+                    if self.url and self.title:
+                        return
