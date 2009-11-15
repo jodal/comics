b0dd24d928f057a76fb0a8632718e47f2e7672e8
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-15 23:15:18 +0100

    Add end_date to lagunen crawler

--- comics/crawler/crawlers/lagunen.py
+++ comics/crawler/crawlers/lagunen.py
@@ -6,6 +6,7 @@ class ComicMeta(BaseComicMeta):
     language = 'no'
     url = 'http://www.start.no/tegneserier/lagunen/'
     start_date = '1991-05-13'
+    end_date = '2009-06-24' # No longer hosted at start.no
     history_capable_days = 30
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 1
