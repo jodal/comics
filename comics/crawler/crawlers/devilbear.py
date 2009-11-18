980f9b3620d6a27be2b2abd41a9a4db892a61f3a
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-18 22:21:52 +0100

    Add crawler for devilbear

---
+++ comics/crawler/crawlers/devilbear.py
@@ -0,0 +1,18 @@
+from comics.crawler.base import BaseComicCrawler
+from comics.crawler.meta import BaseComicMeta
+from comics.crawler.utils.lxmlparser import LxmlParser
+
+class ComicMeta(BaseComicMeta):
+    name = 'The Devil Bear'
+    language = 'en'
+    url = 'http://www.thedevilbear.com/'
+    start_date = '2009-01-01'
+    history_capable_days = 0
+    schedule = 'Mo'
+    time_zone = -8
+    rights = 'Ben Bourbon'
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        page = LxmlParser('http://www.thedevilbear.com/')
+        self.url = page.src('#cg_img img')
