caa85643122dfbc0337d1f0cae414707d93ebd90
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-25 23:20:34 +0100

    Add crawler for 'Super Effective'

---
+++ comics/crawlers/supereffective.py
@@ -0,0 +1,19 @@
+import datetime as dt
+
+from comics.crawler.base import BaseComicCrawler
+from comics.crawler.meta import BaseComicMeta
+
+class ComicMeta(BaseComicMeta):
+    name = 'Super Effective'
+    language = 'en'
+    url = 'http://www.vgcats.com/super/'
+    start_date = '2008-04-23'
+    history_capable_date = '2008-04-23'
+    time_zone = -5
+    rights = 'Scott Ramsoomair'
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.url = 'http://www.vgcats.com/super/images/%(date)s.gif' % {
+            'date': self.pub_date.strftime('%y%m%d'),
+        }
