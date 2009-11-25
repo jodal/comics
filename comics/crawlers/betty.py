5e5294c27d251a647467670f857e27abbd819972
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-25 22:56:23 +0100

    Add crawler for 'Betty'

---
+++ comics/crawlers/betty.py
@@ -0,0 +1,16 @@
+from comics.crawler.base import BaseComicsComComicCrawler
+from comics.crawler.meta import BaseComicMeta
+
+class ComicMeta(BaseComicMeta):
+    name = 'Get Fuzzy'
+    language = 'en'
+    url = 'http://comics.com/betty/'
+    start_date = '1991-01-01'
+    history_capable_date = '2008-10-13'
+    schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
+    time_zone = -5
+    rights = 'Delainey & Gerry Rasmussen'
+
+class ComicCrawler(BaseComicsComComicCrawler):
+    def _get_url(self):
+        self._get_url_helper('Betty')
