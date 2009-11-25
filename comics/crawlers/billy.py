981f6d134d4fcae30c1ddf3717a747287271605d
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-25 22:26:41 +0100

    Add crawler for 'Billy'

---
+++ comics/crawlers/billy.py
@@ -0,0 +1,18 @@
+from comics.crawler.base import BaseComicCrawler
+from comics.crawler.meta import BaseComicMeta
+
+class ComicMeta(BaseComicMeta):
+    name = 'Billy'
+    language = 'no'
+    url = 'http://www.billy.no/'
+    start_date = '1950-01-01'
+    history_capable_days = 6
+    schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
+    time_zone = 1
+    rights = 'Mort Walker'
+
+class ComicCrawler(BaseComicCrawler):
+    def _get_url(self):
+        self.url = 'http://cserver.it-content.com/retriever.php?id=104&date=%(date)s' % {
+            'date': self.pub_date.strftime('%Y%m%d'),
+        }
