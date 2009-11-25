fed8d1d85b929dc21cd49cd2cd0ac660f19e7a36
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-25 22:41:09 +0100

    Update 'Bizarro' crawler to use msn.no instead of start.no

--- comics/crawlers/bizarro.py
+++ comics/crawlers/bizarro.py
@@ -4,16 +4,18 @@ from comics.crawler.meta import BaseComicMeta
 class ComicMeta(BaseComicMeta):
     name = 'Bizarro'
     language = 'no'
-    url = 'http://www.start.no/tegneserier/bizarro/'
+    url = 'http://underholdning.no.msn.com/tegneserier/bizarro/'
     start_date = '1985-01-01'
-    end_date = '2009-06-24' # No longer hosted at start.no
-    history_capable_days = 30
+    history_capable_days = 12
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 1
     rights = 'Dan Piraro'
 
 class ComicCrawler(BaseComicCrawler):
     def _get_url(self):
-        self.url = 'http://g2.start.no/tegneserier/striper/bizarro/biz-striper/biz%(date)s.gif' % {
-            'date': self.pub_date.strftime('%Y%m%d'),
-        }
+        self.parse_feed('http://underholdning.no.msn.com/rss/bizarro.aspx')
+
+        for entry in self.feed.entries:
+            if self.timestamp_to_date(entry.updated_parsed) == self.pub_date:
+                self.url = entry.enclosures[0].href
+                return
