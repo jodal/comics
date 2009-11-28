2e54ae88dca2c355fbb84449db1975f27b75d30e
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-11-28 14:55:38 +0100

    Update wulffmorgenthalerap crawler to use web instead of their broken feed

--- comics/crawlers/wulffmorgenthalerap.py
+++ comics/crawlers/wulffmorgenthalerap.py
@@ -1,7 +1,6 @@
-import re
-
 from comics.crawler.base import BaseComicCrawler
 from comics.crawler.meta import BaseComicMeta
+from comics.crawler.utils.lxmlparser import LxmlParser
 
 class ComicMeta(BaseComicMeta):
     name = 'Wulffmorgenthaler (ap.no)'
@@ -15,28 +14,6 @@ class ComicMeta(BaseComicMeta):
 
 class ComicCrawler(BaseComicCrawler):
     def _get_url(self):
-        self.parse_feed(
-            'http://www.aftenposten.no/eksport/rss-1_0/'
-            '?seksjon=tegneserier&utvalg=siste')
-
-        # XXX: All entries in the feed got the same date, so we just check the
-        # first one, and let the later checksumming of the strip image take
-        # care of duplicates.
-        for entry in self.feed.entries:
-            if entry.title == 'Dagens Wulffmorgenthaler':
-               self.web_url = entry.link
-               break
-
-        if self.web_url is None:
-            return
-
-        self.parse_web_page()
-
-        strip_pattern = re.compile(
-            r'.*/_(Escenic-)*[Ww]t_\w+_\d{1,2}_\d{6,7}x.jpg$')
-        for image in self.web_page.imgs:
-            if 'src' in image:
-                matches = strip_pattern.match(image['src'])
-                if matches is not None:
-                    self.url = self.join_web_url(image['src'])
-                    return
+        self.web_url = 'http://www.aftenposten.no/tegneserier/'
+        page = LxmlParser(self.web_url)
+        self.url = page.src('img#theCartoon')
