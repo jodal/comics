a6e48f4c27fd934e22833b3823d1a049a85bbd8d
Thomas Adamcik <adamcik@samfundet.no>
2009-12-03 21:10:53 +0800

    Add crawler for Abstruse Goose

---
+++ comics/comics/abstrusegoose.py
@@ -0,0 +1,21 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerResult
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'Abstruse Goose'
+    language = 'en'
+    url = 'http://www.abstrusegoose.com/'
+    start_date = '2008-02-01'
+    rights = 'lcfr, CC BY-NC 3.0 US'
+
+class Crawler(CrawlerBase):
+    history_capable_days = 10
+    schedule = 'Mo,We,Fr'
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://abstrusegoose.com/feed/atom')
+        for entry in feed.for_date(pub_date):
+            url = entry.content0.src('img[src*="/strips/"]')
+            title = entry.title
+            text = entry.content0.title('img[src*="/strips/"]')
+            return CrawlerResult(url, title, text)
