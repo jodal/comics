e7473e740e9dd883900939ab22ac0f25f46aff68
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-12-14 23:22:35 +0100

    Fix django.contrib.sessions.tests by setting a CACHE_BACKEND for testing

--- comics/settings/testing.py
+++ comics/settings/testing.py
@@ -4,3 +4,6 @@ from comics.settings.dev import *
 # django.contrib.{admin,auth,session} unit tests.
 MIDDLEWARE_CLASSES = [i for i in MIDDLEWARE_CLASSES
     if not i.startswith('django.middleware.cache')]
+
+# Use a memory cache backend for django.contrib.sessions.tests tests
+CACHE_BACKEND = 'locmem:///?timeout=300&max_entries=1000'
