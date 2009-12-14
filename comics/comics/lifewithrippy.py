b02dc8338ee7514bba30acba32df4753a1fc1b20
Kristian Klette <klette@samfundet.no>
2009-12-14 12:16:36 +0100

    Two new crawlers.
    * Added Life With Rippy
    * Added Rhymes With Witch

---
+++ comics/comics/lifewithrippy.py
@@ -0,0 +1,19 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerResult
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'Life With Rippy'
+    language = 'en'
+    url = 'http://www.rhymes-with-witch.com/'
+    start_date = '2006-08-09'
+    rights = 'r*k*milholland '
+
+class Crawler(CrawlerBase):
+    history_capable_date = '2006-08-09'
+
+    def crawl(self, pub_date):
+        page_url = 'http://www.rhymes-with-witch.com/lwr%s.shtml' % (
+            pub_date.strftime('%m%d%Y'),)
+        page = self.parse_page(page_url)
+        url = page.src('div > img')
+        return CrawlerResult(url)
