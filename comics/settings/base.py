12034e0a736fbe43bb88b681ce04463358a0aeca
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-12-25 14:23:55 +0100

    Blacklist another cyanideandhappiness 'comic on its way' image

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -118,6 +118,7 @@ COMICS_STRIP_BLACKLIST = (
     # Dagbladet.no
     '61c66a1c84408df5b855004dd799d5e59f4af99f4c6fe8bf4aabf8963cab7cb5',
     # Cyanide and Happiness
+    '01237a79e2a283718059e4a180a01e8ffa9f9b36af7e0cad5650dd1a08665971',
     '181e7d11ebd3224a910d9eba2995349da5d483f3ae9643a2efe4f7dd3d9f668d',
     '6dec8be9787fc8b103746886033ccad7348bc4eec44c12994ba83596f3cbcd32',
     'f56248bf5b94b324d495c3967e568337b6b15249d4dfe7f9d8afdca4cb54cd29',
