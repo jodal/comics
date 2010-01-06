e3c466bff2ba6fe45e808a592a9ab9f8232925a6
Thomas Adamcik <adamcik@samfundet.no>
2010-01-06 11:12:19 +0100

    Add Evil Inc crawler

---
+++ comics/comics/evilinc.py
@@ -0,0 +1,28 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerResult
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'Evil Inc.'
+    language = 'en'
+    url = 'http://www.evil-comic.com/'
+    start_date = '2005-05-30'
+    rights = 'Brad J. Guigar - Colorist: Ed Ryzowski'
+
+class Crawler(CrawlerBase):
+    history_capable_date = '2005-05-30'
+    schedule = 'Mo,Tu,We,Th,Fr,Sa'
+
+    def crawl(self, pub_date):
+        page_url = 'http://www.evil-comic.com/archive/%s.html' % (
+            pub_date.strftime('%Y%m%d'))
+
+        page = self.parse_page(page_url)
+
+        # Page does not generate 404, redirects to archive page instead
+        if page.url() != page_url:
+            return
+
+        url = page.src('#ei_strip')
+        title = page.text('#seriesselect option[selected]')
+
+        return CrawlerResult(url, title)
