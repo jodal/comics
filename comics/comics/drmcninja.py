4c94976d89ed9af9d0040aa37e74a7d23c1e7c09
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-01-06 22:14:32 +0100

    Add crawler for drmcninja

---
+++ comics/comics/drmcninja.py
@@ -0,0 +1,25 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerResult
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'The Adventures of Dr. McNinja'
+    language = 'en'
+    url = 'http://drmcninja.com/'
+    start_date = '2004-08-03'
+    rights = 'Christopher Hastings'
+
+class Crawler(CrawlerBase):
+    history_capable_days = 32
+    schedule = 'Mo,We,Fr'
+    time_zone = -5
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://drmcninja.com/feed')
+        for entry in feed.for_date(pub_date):
+            if not '/comic/' in entry.link:
+                continue
+            url = entry.summary.src('img[src*="/comics-rss/"]')
+            url = url.replace('comics-rss', 'comics')
+            title = entry.title
+            text = entry.summary.title('img[src*="/comics-rss/"]')
+            return CrawlerResult(url, title, text)
