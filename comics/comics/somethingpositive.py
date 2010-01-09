593db91e182b97d097473896d464cd0c83c1a258
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-01-09 20:54:53 +0100

    Update crawler for somethingpositive

--- comics/comics/somethingpositive.py
+++ comics/comics/somethingpositive.py
@@ -13,6 +13,6 @@ class Crawler(CrawlerBase):
     schedule = 'Mo,Tu,We,Th,Fr'
 
     def crawl(self, pub_date):
-        url = 'http://www.somethingpositive.net/arch/sp%s.gif' % (
+        url = 'http://www.somethingpositive.net/sp%s.gif' % (
             pub_date.strftime('%m%d%Y'),)
         return CrawlerResult(url)
