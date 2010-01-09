7a201d4c62ef32a03b95b9f220cfb37419debba2
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-01-09 20:46:41 +0100

    Rewrite extralife crawler to use the feed and store titles too

--- comics/comics/extralife.py
+++ comics/comics/extralife.py
@@ -9,11 +9,19 @@ class Meta(MetaBase):
     rights = 'Scott Johnson'
 
 class Crawler(CrawlerBase):
-    history_capable_date = '2001-06-17'
+    history_capable_days = 10
     schedule = 'Mo,We,Fr'
     time_zone = -7
 
     def crawl(self, pub_date):
-        url = 'http://www.myextralife.com/comics/%s.jpg' % (
-            pub_date.strftime('%Y-%m-%d'),)
-        return CrawlerResult(url)
+        feed = self.parse_feed('http://www.myextralife.com/feed/')
+        for entry in feed.for_date(pub_date):
+            if not '/comic/' in entry.link:
+                continue
+            url = entry.summary.src('img[src*="/comics-rss/"]')
+            url = url.replace('/comics-rss/', '/comics/')
+            title = entry.summary.title('img[src*="/comics-rss/"]')
+            title = title.replace('Comic: ', '')
+            title = title.replace(u'\u201c', '')
+            title = title.replace(u'\u201d', '')
+            return CrawlerResult(url, title)
