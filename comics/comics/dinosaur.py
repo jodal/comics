26210f000b66a322e8793c8a87e9493b0b2d637f
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-01-17 19:27:45 +0100

    Add crawler for 'dinosaur'

---
+++ comics/comics/dinosaur.py
@@ -0,0 +1,22 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'Dinosaur Comics'
+    language = 'en'
+    url = 'http://www.qwantz.com/'
+    start_date = '2003-02-01'
+    rights = 'Ryan North'
+
+class Crawler(CrawlerBase):
+    history_capable_days = 32
+    schedule = 'Mo,Tu,We,Th,Fr'
+    time_zone = -5
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://www.rsspect.com/rss/qwantz.xml')
+        for entry in feed.for_date(pub_date):
+            url = entry.summary.src('img[src*="/comics/"]')
+            title = entry.title
+            text = entry.summary.title('img[src*="/comics/"]')
+            return CrawlerImage(url, title, text)
