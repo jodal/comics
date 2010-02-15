1716bec5615d830e63618d3bdacf9feccd2842ec
Jim Wyllie <jwyllie83@gmail.com>
2010-02-15 17:25:13 -0500

    Added F Minus

---
+++ comics/comics/fminus.py
@@ -0,0 +1,17 @@
+from comics.aggregator.crawler import ComicsComCrawlerBase
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'F Minus'
+    language = 'en'
+    url = 'http://comics.com/f_minus/'
+    start_date = '1999-09-01'
+    rights = 'Tony Carrillo'
+
+class Crawler(ComicsComCrawlerBase):
+    history_capable_date = '2001-02-02'
+    schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
+    time_zone = -5
+
+    def crawl(self, pub_date):
+        return self.crawl_helper('F Minus', pub_date)
