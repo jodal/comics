2110b6c94937092eb857c818a0fe23ea5f602199
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-02-16 19:23:57 +0100

    Cleanup 'thedreamer' crawler

--- comics/comics/thedreamer.py
+++ comics/comics/thedreamer.py
@@ -5,15 +5,15 @@ from comics.core.models import Release
 class Meta(MetaBase):
     name = 'The Dreamer'
     language = 'en'
-    url = 'http://thedreamercomic.com'
-    rights = 'Lora Innes, Innes Art LLC. All rights reserved.'
+    url = 'http://thedreamercomic.com/'
+    rights = 'Lora Innes'
 
 class Crawler(CrawlerBase):
+    schedule = 'Fr'
+    multiple_releases_per_day = True
+
     def crawl(self, pub_date):
-        next_id = Release.objects.filter(comic__name='The Dreamer').count() + 1
-        url = 'http://thedreamercomic.com/comic.php?id=%s' % next_id
-        print url
-        page = self.parse_page(url)
-        img_url = page.src('img[src*="issues/"]')
-        img_alt = page.alt('img[src*="issues/"]')
-        return CrawlerImage(img_url, img_alt)
+        page = self.parse_page('http://thedreamercomic.com/comic.php')
+        url = page.src('img[src*="issues/"]')
+        title = page.alt('img[src*="issues/"]')
+        return CrawlerImage(url, title)
