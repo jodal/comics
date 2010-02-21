498c5dbc2d9d874af7866f4dab63da12092ed076
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-02-21 14:47:37 +0100

    Add tests for Crawler.current_date and time zone handling

--- comics/aggregator/tests/__init__.py
+++ comics/aggregator/tests/__init__.py
@@ -2,5 +2,6 @@ import logging
 import os
 
 from comics.aggregator.tests.command import *
+from comics.aggregator.tests.crawler import *
 
 logging.basicConfig(level=logging.CRITICAL, filename=os.devnull)
