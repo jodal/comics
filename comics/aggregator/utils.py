fa87821a4a4b282e5cc0d9311a0c4dfb5fbc37db
Thomas Adamcik <adamcik@samfundet.no>
2010-03-01 23:27:11 +0100

    Add helper for getting schedule

---
+++ comics/aggregator/utils.py
@@ -0,0 +1,11 @@
+from comics.comics import get_comic_module
+
+SCHEDULE_DAYS = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+
+def get_comic_schedule(comic):
+    module = get_comic_module(comic.slug)
+    schedule = module.Crawler(comic).schedule
+
+    if not schedule:
+        return []
+    return [SCHEDULE_DAYS.index(day) for day in schedule.split(',')]
