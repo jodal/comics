47243f0edcbb0b05fff1cd0baadf135198f82d5a
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-03-04 23:15:59 +0100

    Revert "Cache the status page for one hour. Quite expensive page"
    This reverts commit 8a2bb8784c386cc8f3436cdaff59d97f1c6a40a8.
    
    As the page is mainly for admin use, it is more important that it is fresh than
    fast.

--- comics/core/urls.py
+++ comics/core/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.decorators.cache import cache_page
 
 from comics.core import views
 
@@ -37,7 +36,7 @@ urlpatterns = patterns('',
         views.comic_show, name='comic-date-days'),
 
     # Status page
-    url(r'^status/$', cache_page(views.status, 3600), name='status'),
+    url(r'^status/$', views.status, name='status'),
 
     # About page
     url(r'^about/$', views.about, name='about'),
