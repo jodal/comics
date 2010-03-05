05abbec8e80b89d5cac1a54bd93d041945111c0d
Thomas Adamcik <adamcik@samfundet.no>
2010-03-05 13:36:27 +0100

    Style fixes

--- comics/comics/whiteninja.py
+++ comics/comics/whiteninja.py
@@ -10,8 +10,8 @@ class Meta(MetaBase):
 
 class Crawler(CrawlerBase):
     history_capable_days = 60
+    schedule = 'Mo,We,Fr'
     time_zone = -6
-    schedule = "Mo,We,Fr"
 
     def crawl(self, pub_date):
         feed = self.parse_feed(
