2302a26939d349853e4f955e57beb386e3efc6a6
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-03-07 20:55:00 +0100

    Update some release schedules

--- comics/comics/countyoursheep.py
+++ comics/comics/countyoursheep.py
@@ -10,7 +10,6 @@ class Meta(MetaBase):
 
 class Crawler(CrawlerBase):
     history_capable_date = '2003-06-11'
-    schedule = 'Mo,Tu,We,Th,Fr'
 
     def crawl(self, pub_date):
         page_url = 'http://www.countyoursheep.com/d/%s.html' % (
