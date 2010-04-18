6c944bbb357c4f6a8dab2b79ab4fb5780a10dc6b
Thomas Adamcik <adamcik@samfundet.no>
2010-04-18 04:57:35 +0200

    Update 'lunchstriper' to use directory listing and extract image name

--- comics/comics/lunchstriper.py
+++ comics/comics/lunchstriper.py
@@ -1,3 +1,6 @@
+import re
+import urllib
+
 from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.comics.lunch import Meta as LunchMeta
 
@@ -7,11 +10,22 @@ class Meta(LunchMeta):
 
 class Crawler(CrawlerBase):
     time_zone = 1
-    history_capable_days = 21
+    history_capable_date = '2009-04-01'
 
     def crawl(self, pub_date):
-        feed = self.parse_feed('http://lunchstriper.lunddesign.no/?feed=rss2')
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/comics/"]')
-            return CrawlerImage(url)
+        page = self.parse_page('http://lunchstriper.lunddesign.no/comics/')
+        url = page.href('a[href*="comics/%s"]' % pub_date.strftime('%Y-%m-%d'))
 
+        if not url:
+            return
+
+        title = re.sub('^http://lunchstriper.lunddesign.no/comics/' +
+            '\d{4}-\d{2}-\d{2}(-LUNCH_\d+)?-?', '', url or '')
+        title = re.sub('(_farger)?\.png$', '', title)
+        title = urllib.unquote(title).encode('iso-8859-1').decode('utf-8')
+        title = title.replace('_', ' ').strip()
+
+        if title:
+            title = title[0].upper() + title[1:]
+
+        return CrawlerImage(url, title or None)
