f0fd61f0d4ebe5f9f565bdeb594203c2be7c1694
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-07-26 13:03:42 +0200

    Make 'ctrlaltdel' work, again

--- comics/comics/ctrlaltdel.py
+++ comics/comics/ctrlaltdel.py
@@ -13,8 +13,12 @@ class Crawler(CrawlerBase):
     schedule = 'Mo,We,Fr'
     time_zone = -5
 
+    # Without User-Agent set, the server returns empty responses
+    headers = {'User-Agent': 'Mozilla/4.0'}
+
     def crawl(self, pub_date):
         page = self.parse_page('http://www.cad-comic.com/cad/%s' %
             pub_date.strftime('%Y%m%d'))
         url = page.src('img[src*="/comics/"]')
+        title = page.alt('img[src*="/comics/"]')
         return CrawlerImage(url)
