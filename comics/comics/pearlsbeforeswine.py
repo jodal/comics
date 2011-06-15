832abce15964fc303246d4090aae58892cf9607b
Jim Wyllie <jwyllie83@gmail.com>
2011-06-14 22:43:44 -0400

    Bulk update comics.com to gocomics.com

--- comics/comics/pearlsbeforeswine.py
+++ comics/comics/pearlsbeforeswine.py
@@ -1,14 +1,14 @@
-from comics.aggregator.crawler import ComicsComCrawlerBase
+from comics.aggregator.crawler import GoComicsComCrawlerBase
 from comics.meta.base import MetaBase
 
 class Meta(MetaBase):
     name = 'Pearls Before Swine'
     language = 'en'
-    url = 'http://comics.com/pearls_before_swine/'
+    url = 'http://www.gocomics.com/pearlsbeforeswine/'
     start_date = '2001-12-30'
     rights = 'Stephan Pastis'
 
-class Crawler(ComicsComCrawlerBase):
+class Crawler(GoComicsComCrawlerBase):
     history_capable_date = '2002-01-06'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = -5
--- comics/comics/roseisrose.py
+++ comics/comics/roseisrose.py
@@ -1,14 +1,14 @@
-from comics.aggregator.crawler import ComicsComCrawlerBase
+from comics.aggregator.crawler import GoComicsComCrawlerBase
 from comics.meta.base import MetaBase
 
 class Meta(MetaBase):
     name = 'Rose Is Rose'
     language = 'en'
-    url = 'http://comics.com/rose_is_rose/'
+    url = 'http://www.gocomics.com/roseisrose/'
     start_date = '1984-10-02'
     rights = 'Pat Brady'
 
-class Crawler(ComicsComCrawlerBase):
+class Crawler(GoComicsComCrawlerBase):
     history_capable_date = '1995-10-09'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = -5
