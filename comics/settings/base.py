18c68f2e08b0799dc29e7238dd96aae1ff2ff313
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-07-15 23:21:22 +0200

    Switch to class-based template loaders

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -46,7 +46,7 @@ ADMIN_MEDIA_ROOT = os.path.join(DJANGO_DIR, 'contrib/admin/media/')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -87,6 +87,29 @@ INSTALLED_APPS = (
     'comics.sets',
     'comics.utils',
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 # Caching
 CACHE_BACKEND = 'locmem:///?timeout=300&max_entries=1000'
