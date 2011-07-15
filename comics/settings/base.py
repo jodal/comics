c4e8fc14633ebe28181603aaf52151a6f77449e5
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-07-15 23:50:57 +0200

    Add static context_processor

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -94,6 +94,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'comics.core.context_processors.site_settings',
     'comics.core.context_processors.all_comics',
 )
