3f7cc44ee590d40707155f1ae73d33ba792a98d1
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-08-14 02:52:30 +0200

    Fix path to ADMIN_MEDIA

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -55,7 +55,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
