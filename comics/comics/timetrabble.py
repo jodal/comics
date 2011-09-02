89de0267eb4fe9af97ab01e928f2737438be7d7c
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-09-03 01:02:54 +0200

    Add 'timetrabble' crawler (fixes #20)

---
+++ comics/comics/timetrabble.py
@@ -0,0 +1,23 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.meta.base import MetaBase
+
+class Meta(MetaBase):
+    name = 'Time Trabble'
+    language = 'en'
+    url = 'http://timetrabble.com/'
+    start_date = '2010-05-09'
+    rights = 'Mikey Heller'
+
+class Crawler(CrawlerBase):
+    history_capable_days = 90
+    time_zone = -5
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://timetrabble.com/?feed=rss2')
+        for entry in feed.for_date(pub_date):
+            url = entry.summary.src('img.comicthumbnail')
+            if not url:
+                continue
+            url = url.replace('comics-rss', 'comics')
+            title = entry.title
+            return CrawlerImage(url, title)
--- docs/changes.rst
+++ docs/changes.rst
@@ -10,6 +10,7 @@ v1.1.2 (in development)
 
 **Crawlers**
 
+- New: ``timetrabble``
 - Update ``pennyarcade`` after site change.
 
 
