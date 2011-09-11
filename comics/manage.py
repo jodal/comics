26393240cdc15c34cda04a070b224d4400be4b75
Jim Wyllie <jwyllie83@gmail.com>
2011-09-11 12:21:15 -0400

    Converted manage.py to use the new virt library

--- comics/manage.py
+++ comics/manage.py
@@ -5,6 +5,10 @@ import sys
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(PROJECT_DIR, '../'))
 
+# Respect a virtualenv if we need to
+from comics.utils.virtualenv import enter_virtualenv
+enter_virtualenv()
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
