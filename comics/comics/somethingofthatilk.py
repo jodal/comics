4216b0bfa4e0b48c8efbc3588afb59052a26f3d7
Jim Frode Hoff <jimfrodehoff@gmail.com>
2011-11-26 22:38:21 +0100

    Added crawler for SOmthing of that Ilk

--- comics/comics/somethingofthatilk.py
+++ comics/comics/somethingofthatilk.py
@@ -4,18 +4,20 @@ from comics.meta.base import MetaBase
 class Meta(MetaBase):
     name = 'Something of that ilk'
     language = 'en'
-    url = 'http://somethingofthatilk.com/'
+    url = 'http://www.somethingofthatilk.com/'
     start_date = '2011-02-19'
     rights = 'Ty Devries'
 
 class Crawler(CrawlerBase):
-    history_capable_date = '2011-02-19'
+    history_capable_days = 1
     schedule = 'Mo,Tu,We,Th,Fr'
     time_zone = -5
 
     def crawl(self, pub_date):
-        feed = self.parse_feed('http://somethingofthatilk.com/rss/index.php')
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/comics/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+        page = self.parse_page('http://www.somethingofthatilk.com/')
+        url = 'http://www.somethingofthatilk.com/'
+        url = page.src('img[src*="/comics/"]')
+        
+        print '\n\n', url, '\n\n'        
+
+        return CrawlerImage(url)
