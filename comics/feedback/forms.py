ca3366bfdec91797c0a5406a5ba8094c4d13a233
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-01-18 14:30:29 +0100

    Fix typo in feedback form help text

--- comics/feedback/forms.py
+++ comics/feedback/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class FeedbackForm(BootstrapForm):
     message = forms.CharField(label="What's on your heart",
-        help_text='Remember to sign with you mail address if you want a reply.',
+        help_text='Sign with your email address if you want a reply.',
         widget=forms.Textarea(attrs={'rows': 5, 'cols': 100}))
