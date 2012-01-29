60019891dcff24ca64b368085a935afb47147ba5
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-01-29 20:13:53 +0100

    Order comics on status page by slug

--- comics/core/views.py
+++ comics/core/views.py
@@ -129,7 +129,7 @@ def status(request, days=21):
     releases = releases.select_related('comic__slug')
     releases = releases.order_by('comic__slug').distinct()
 
-    for comic in Comic.objects.filter(active=True):
+    for comic in Comic.objects.filter(active=True).order_by('slug'):
         schedule = get_comic_schedule(comic)
         timeline[comic] = []
 
