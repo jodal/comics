379c6254da0d6a06f8c01cd7cd2632a1d59624ac
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-03-12 23:06:30 +0100

    Use request.user_set in context preprocessor

--- comics/sets/context_processors.py
+++ comics/sets/context_processors.py
@@ -1,11 +1,8 @@
-from comics.sets.models import UserSet
-
 def user_set(request):
-    try:
-        user_set = UserSet.objects.get(user=request.user)
+    if hasattr(request, 'user_set'):
         return {
-            'user_set': user_set,
-            'user_set_comics': user_set.comics.all(),
+            'user_set': request.user_set,
+            'user_set_comics': request.user_set.comics.all(),
         }
-    except UserSet.DoesNotExist:
+    else:
         return {}
