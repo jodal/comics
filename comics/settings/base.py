7700d6c4afbcbf3d9e4ffbd0c0d05332119e16c8
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-03-24 20:49:13 +0100

    Add email logging filter. Fixes deprecation warning

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -131,9 +131,15 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
