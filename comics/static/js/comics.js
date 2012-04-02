6f919a5d446575a0b90a79694e4e35b92e22d69c
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-04-02 10:42:47 +0200

    Only capture hjkl keys on pages with #releases

--- comics/static/js/comics.js
+++ comics/static/js/comics.js
@@ -43,18 +43,20 @@ var keyboardNavigation = (function () {
     };
 
     return function (event) {
-        if (event.which == 72 || event.which == 104) { // H or h
-            event.preventDefault();
-            goToPreviousPage();
-        } else if (event.which == 74 || event.which == 106) { // J or j
-            event.preventDefault();
-            goToNextRelease();
-        } else if (event.which == 75 || event.which == 107) { // K or k
-            event.preventDefault();
-            goToPreviousRelease();
-        } else if (event.which == 76 || event.which == 108) { // L or l
-            event.preventDefault();
-            goToNextPage();
+        if ($('#releases').length) {
+            if (event.which == 72 || event.which == 104) { // H or h
+                event.preventDefault();
+                goToPreviousPage();
+            } else if (event.which == 74 || event.which == 106) { // J or j
+                event.preventDefault();
+                goToNextRelease();
+            } else if (event.which == 75 || event.which == 107) { // K or k
+                event.preventDefault();
+                goToPreviousRelease();
+            } else if (event.which == 76 || event.which == 108) { // L or l
+                event.preventDefault();
+                goToNextPage();
+            }
         }
     };
 })();
