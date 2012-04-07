20211a9494cc4ecd3f50bf1280d034da8f0cda50
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-04-07 23:57:42 +0200

    Fix secret key generation for superuser created with mangage.py

--- comics/accounts/models.py
+++ comics/accounts/models.py
@@ -11,21 +11,21 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 
+def make_secret_key():
+    return uuid.uuid4().hex
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     secret_key = models.CharField(max_length=32, blank=False,
+        default=make_secret_key,
         help_text='Secret key for feed and API access')
 
     class Meta:
         db_table = 'comics_user_profile'
 
-    def __init__(self, *args, **kwargs):
-        super(UserProfile, self).__init__(*args, **kwargs)
-        if self.secret_key is None:
-            self.generate_new_secret_key()
-
     def __unicode__(self):
         return u'User profile for %s' % self.user
 
     def generate_new_secret_key(self):
-        self.secret_key = uuid.uuid4().hex
+        self.secret_key = make_secret_key()
