26ccafe7dd2cd017e3df3626bdec82597504815e
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-07-11 23:37:20 +0200

    Group resources in an API

--- comics/api/urls.py
+++ comics/api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import include, patterns
 
+from tastypie.api import Api
+
 from comics.api.resources import ComicResource
 
-comic_resource = ComicResource()
+v1_api = Api(api_name='v1')
+v1_api.register(ComicResource())
 
 urlpatterns = patterns('',
-    (r'', include(comic_resource.urls)),
+    (r'', include(v1_api.urls)),
 )
