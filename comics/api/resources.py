168afc80923e19b54e246c989082bf3f55dd2a5a
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-07-12 23:53:20 +0200

    Add ImageResource and ReleaseResource, with relations and some access and filtering setup

--- comics/api/resources.py
+++ comics/api/resources.py
@@ -1,8 +1,9 @@
-from tastypie.constants import ALL
+from tastypie import fields
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
 
 from comics.api.authentication import SecretKeyAuthentication
-from comics.core.models import Comic
+from comics.core.models import Comic, Release, Image
 
 
 class ComicResource(ModelResource):
@@ -17,4 +18,39 @@ class ComicResource(ModelResource):
             'language': 'exact',
             'name': ALL,
             'slug': ALL,
+        }
+
+
+class ImageResource(ModelResource):
+    class Meta:
+        queryset = Image.objects.all()
+        resource_name = 'image'
+        authentication = SecretKeyAuthentication()
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+        filtering = {
+            'fetched': ALL,
+            'title': ALL,
+            'text': ALL,
+            'height': ALL,
+            'width': ALL,
+        }
+
+
+class ReleaseResource(ModelResource):
+    comic = fields.ToOneField(ComicResource, 'comic')
+    images = fields.ToManyField(ImageResource, 'images', full=True)
+
+    class Meta:
+        queryset = Release.objects.select_related(depth=1
+            ).order_by('-fetched')
+        resource_name = 'release'
+        authentication = SecretKeyAuthentication()
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+        filtering = {
+            'comic': ALL_WITH_RELATIONS,
+            'images': ALL_WITH_RELATIONS,
+            'pub_date': ALL,
+            'fetched': ALL,
         }
--- comics/api/urls.py
+++ comics/api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls.defaults import include, patterns
 
 from tastypie.api import Api
 
-from comics.api.resources import ComicResource
+from comics.api.resources import ComicResource, ImageResource, ReleaseResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(ComicResource())
+v1_api.register(ImageResource())
+v1_api.register(ReleaseResource())
 
 urlpatterns = patterns('',
     (r'', include(v1_api.urls)),
