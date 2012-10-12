3ede1645b21c9ef92d4d2e096aa8e1b01bcc6117
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-10-13 00:28:02 +0200

    Add custom test runner by carljm
    `python manage.py test` now only runs comics' own tests

--- comics/aggregator/tests/__init__.py
+++ comics/aggregator/tests/__init__.py
@@ -1,7 +1,4 @@
 import logging
 import os
 
-from comics.aggregator.tests.command import *  # NOQA
-from comics.aggregator.tests.crawler import *  # NOQA
-
 logging.basicConfig(level=logging.CRITICAL, filename=os.devnull)
