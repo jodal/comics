a5772726e6f1727191f69990e80303f30def6544
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-10-13 02:25:05 +0200

    The testing settings is redundant since the default DB is Sqlite

--- comics/settings/testing.py
+++
@@ -1,9 +0,0 @@
-from comics.settings.dev import *  # NOQA
-
-# Use in-memory Sqlite3 database for testing to reduce startup time
-DATABASES = {
-    'default': {
-        'NAME': None,
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
-}
