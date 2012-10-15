e2b77a2c98cbd51c5c0546e4146dd60af0a64c86
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-10-15 23:35:18 +0200

    Add subscription count to comics profile list

--- comics/accounts/admin.py
+++ comics/accounts/admin.py
@@ -12,8 +12,12 @@ def email(obj):
     return obj.user.email
 
 
+def subscription_count(obj):
+    return obj.comics.count()
+
+
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', email, 'secret_key')
+    list_display = ('user', email, 'secret_key', subscription_count)
     inlines = [SubscriptionInline,]
     readonly_fields = ('user',)
 
