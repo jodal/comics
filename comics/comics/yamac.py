d0764f98762cabe0859daac78818347eadbec540
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-11-16 09:41:58 +0100

    Fix URL

--- comics/comics/yamac.py
+++ comics/comics/yamac.py
@@ -4,7 +4,7 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = 'you and me and cats'
     language = 'en'
-    url = 'http://strawberry-pie.com/yamac/'
+    url = 'http://strawberry-pie.net/yamac/'
     start_date = '2009-07-01'
     rights = 'bubble'
 
