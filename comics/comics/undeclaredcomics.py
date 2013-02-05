813cbdb71d4f9051748c80313d888ca301c37fe4
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-02-05 23:41:41 +0100

    Add crawler for 'undeclaredcomics'

---
+++ comics/comics/undeclaredcomics.py
@@ -0,0 +1,26 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+class ComicData(ComicDataBase):
+    name = 'Undeclared Comics'
+    language = 'en'
+    url = 'http://www.undeclaredcomics.com/'
+    start_date = '2011-08-09'
+    rights = 'Belal'
+
+class Crawler(CrawlerBase):
+    history_capable_days = 180
+    schedule = 'Tu'
+    time_zone = 'US/Eastern'
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://www.undeclaredcomics.com/feed/')
+        for entry in feed.for_date(pub_date):
+            if 'Comics' not in entry.tags:
+                continue
+            url = entry.summary.src('img.comicthumbnail')
+            if not url:
+                continue
+            url = url.replace('comics-rss', 'comics')
+            title = entry.title
+            return CrawlerImage(url, title)
