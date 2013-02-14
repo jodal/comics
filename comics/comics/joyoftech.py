44e0f1220187a0161c58e31b29a3595274414981
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-02-14 10:48:04 +0100

    Update 'joyoftech' after site change

--- comics/comics/joyoftech.py
+++ comics/comics/joyoftech.py
@@ -17,7 +17,10 @@ class Crawler(CrawlerBase):
         feed = self.parse_feed(
             'http://www.joyoftech.com/joyoftech/jotblog/atom.xml')
         for entry in feed.for_date(pub_date):
-            page = self.parse_page(entry.link)
-            url = page.src('img[alt="The Joy of Tech comic"]')
             title = entry.title
+            if not title.startswith('JoT'):
+                continue
+            url = entry.content0.src('img')
+            if url:
+                url = url.replace('superthumb', '')
             return CrawlerImage(url, title)
