b692a001cc7de0a94a6ab74d6c157f404c523481
datagutten <datagutten@datagutten.net>
2013-06-01 22:13:08 +0200

    'veslemoy' is no longer published
    (cherry picked from commit 55674bc6f59260468d503cb6a9f8161058b2610d)

--- comics/comics/veslemoy.py
+++ comics/comics/veslemoy.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -9,16 +9,11 @@ class ComicData(ComicDataBase):
     language = 'no'
     url = 'http://www.side2.no/tegneserie/veslemoy/'
     start_date = '2008-11-14'
+    end_date = '2012-12-31'
+    active = False
     rights = 'Vantina Nina Andreassen'
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = '2008-11-14'
-    schedule = 'Mo,We,Fr'
-    time_zone = 'Europe/Oslo'
-
     def crawl(self, pub_date):
-        url = (
-            'http://pub.tv2.no/nettavisen/tegneserie/' +
-            'pondus/veslemoy/%s.jpg' % pub_date.strftime('%d%m%y'))
-        return CrawlerImage(url)
+        pass  # Comic no longer published
