fea99835d73b994f52a65e9987a2dcdba82b46ce
datagutten <datagutten@datagutten.net>
2013-06-03 15:24:38 +0200

    Renamed the crawler for 'tommy og tigern' and removed 'reform94' Just got confirmed that 'reform94' is inactive

--- comics/comics/tommyogtigern.py
+++ comics/comics/tommyogtigern.py
@@ -1,20 +1,17 @@
-# encoding: utf-8
-
 from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 class ComicData(ComicDataBase):
-    name = 'Reform 94 (heltnormalt.no)'
+    name = 'Tommy og Tigern'
     language = 'no'
-    url = 'http://heltnormalt.no/reform94'
-    start_date = '2012-01-01'
-    rights = 'Jorunn Hanto-Haugse'
+    url = 'http://heltnormalt.no/tommytigern'
+    rights = 'Bill Watterson'
 	
 class Crawler(CrawlerBase):
-    history_capable_date='2013-03-08'
+    history_capable_date='2013-02-01'
     time_zone = 'Europe/Oslo'
 
     def crawl(self, pub_date):
-        url = 'http://heltnormalt.no/img/reform94/%s.jpg' % (
+        url = 'http://heltnormalt.no/img/tommytigern/%s.jpg' % (
             pub_date.strftime('%Y/%m/%d'))
         return CrawlerImage(url)
