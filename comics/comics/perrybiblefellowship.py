64423829cdc141218f841727c72834637366ae40
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-07-05 23:16:49 +0200

    Update 'perrybiblefellowship' to be active again

--- comics/comics/perrybiblefellowship.py
+++ comics/comics/perrybiblefellowship.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -6,11 +6,17 @@ class ComicData(ComicDataBase):
     name = 'The Perry Bible Fellowship'
     language = 'en'
     url = 'http://www.pbfcomics.com/'
-    active = False
     start_date = '2001-01-01'
     rights = 'Nicholas Gurewitch'
 
 
 class Crawler(CrawlerBase):
+    history_capable_date = '2001-01-01'
+    time_zone = 'US/Eastern'
+
     def crawl(self, pub_date):
-        pass  # Comic no longer published
+        feed = self.parse_feed('http://www.pbfcomics.com/feed/feed.xml')
+        for entry in feed.for_date(pub_date):
+            url = entry.summary.src('img[src*="/archive_b/"]')
+            title = entry.title
+            return CrawlerImage(url, title)
