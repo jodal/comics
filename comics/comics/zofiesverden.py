b43b68762b934d0f929de6f0894fc8cf8769e5b8
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-07-06 20:42:49 +0200

    'zofiesverden' is no longer published
    (cherry picked from commit 55674bc6f59260468d503cb6a9f8161058b2610d)

--- comics/comics/zofiesverden.py
+++ comics/comics/zofiesverden.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,17 +7,11 @@ class ComicData(ComicDataBase):
     language = 'no'
     url = 'http://www.zofiesverden.no/'
     start_date = '2006-05-02'
+    end_date = '2012-08-31'
+    active = False
     rights = 'Grethe Nestor & Norunn Blichfeldt Schjerven'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 14
-    schedule = 'Mo,Tu,We,Th,Fr,Sa'
-    time_zone = 'Europe/Oslo'
-
     def crawl(self, pub_date):
-        epoch = self.date_to_epoch(pub_date)
-        url = (
-            'http://www.dagbladet.no/tegneserie/' +
-            'zofiesverdenarkiv/serve.php?%d' % epoch)
-        return CrawlerImage(url)
+        pass  # Comic no longer published
--- docs/changes.rst
+++ docs/changes.rst
@@ -36,6 +36,7 @@ v2.2.0 (in development)
 - Update: ``veslemoy`` is no longer published.
 - Update: ``wulffmorgenthaler`` to work after site change.
 - Update: ``wulffmorgenthalerap`` is no longer active.
+- Update: ``zofiesverden`` is no longer published.
 
 
 v2.1.1 (2013-02-26)
