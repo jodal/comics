5cfaed66aa6cc7a29960870fbcdef867635dce1d
datagutten <datagutten@datagutten.net>
2013-07-06 21:53:09 +0200

    Add crawler for 'truthfacts'

---
+++ comics/comics/truthfacts.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+
+class ComicData(ComicDataBase):
+    name = 'Truth Facts'
+    language = 'no'
+    url = 'http://heltnormalt.no/truthfacts'
+
+
+class Crawler(CrawlerBase):
+    history_capable_date = '2013-02-12'
+    time_zone = 'Europe/Oslo'
+
+    def crawl(self, pub_date):
+        url = 'http://heltnormalt.no/img/truth_facts/%s.jpg' % (
+            pub_date.strftime('%Y/%m/%d'))
+        return CrawlerImage(url)
