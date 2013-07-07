9a3e4affe7ac9644bc18d1e9eca06042a5b8fc38
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-07-07 13:05:34 +0200

    Update 'slagoon' after site change

--- comics/comics/slagoon.py
+++ comics/comics/slagoon.py
@@ -5,17 +5,21 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = "Sherman's Lagoon"
     language = 'en'
-    url = 'http://www.slagoon.com/'
+    url = 'http://shermanslagoon.com/'
     start_date = '1991-05-13'
     rights = 'Jim Toomey'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 32
+    history_capable_date = '2003-12-29'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'US/Eastern'
 
     def crawl(self, pub_date):
-        url = 'http://www.slagoon.com/dailies/SL%s.gif' % (
-            pub_date.strftime('%y%m%d'),)
+        page_url = 'http://shermanslagoon.com/comics/%s-%s-%s/' % (
+            pub_date.strftime('%B').lower(),
+            int(pub_date.strftime('%d')),
+            pub_date.strftime('%Y'))
+        page = self.parse_page(page_url)
+        url = page.src('.entry-content img')
         return CrawlerImage(url)
--- docs/changes.rst
+++ docs/changes.rst
@@ -66,6 +66,7 @@ v2.2.0 (in development)
 - Update: ``pidjin`` to ignore repeated non-comic image.
 - Update: ``reallife`` after site change.
 - Update: ``sheldon`` schedule.
+- Update: ``slagoon`` after site change.
 - Update: ``utensokker`` is published again.
 - Update: ``veslemoy`` is no longer published.
 - Update: ``wulffmorgenthaler`` to work after site change.
