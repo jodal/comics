99cf059e836057a8436203ffe64e8a6db8c30c67
datagutten <datagutten@datagutten.net>
2013-09-21 16:43:30 +0200

    Updated 'wulffmorgenthaler' after site change

--- comics/comics/wulffmorgenthaler.py
+++ comics/comics/wulffmorgenthaler.py
@@ -5,18 +5,19 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = 'Wumo'
     language = 'en'
-    url = 'http://www.wumo.com/'
+    url = 'http://kindofnormal.com/wumo/'
     start_date = '2001-01-01'
     rights = 'Mikael Wulff & Anders Morgenthaler'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 20
+    history_capable_date = '2013-01-15'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'Europe/Copenhagen'
 
     def crawl(self, pub_date):
-        feed = self.parse_feed('http://feeds.feedburner.com/wulffmorgenthaler')
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src^="http://wumo.com/"]')
-            return CrawlerImage(url)
+        page_url = 'http://kindofnormal.com/wumo/'+pub_date.strftime('%Y/%m/%d')
+        page = self.parse_page(page_url)
+        url = 'http://kindofnormal.com/img/wumo/'+pub_date.strftime('%Y/%m/%d')+'.png'
+        title = page.alt('img[src="'+url+'"]')
+        return CrawlerImage(url, title)
