de8e36a344db07d0702951e0ca19d2199d587fe2
Nina Margrethe Smørsgård <nina.m.smorsgard@gmail.com>
2013-10-21 22:07:21 +0200

    created crawler for Pink Parts comic

---
+++ comics/comics/pinkparts.py
@@ -0,0 +1,24 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+
+class ComicData(ComicDataBase):
+    name = 'Pink Parts'
+    language = 'en'
+    url = 'http://pinkpartscomic.com/'
+	start_date = '2010-02-01'
+    rights = 'Katherine Skipper'
+
+
+class Crawler(CrawlerBase):
+	history_capable_date = '2010-02-01'
+	schedule = None
+    time_zone = 'US/Pacific'
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://pinkpartscomic.com/inc/feed.php')
+        for entry in feed.for_date(pub_date):
+			url = entry.sunnary.src('img[src*="/comics/"]')
+			title = entry.title
+			text = entry.summary.alt('img[src*="/comics/"]')
+			return CrawlerImage(url, title, text)
