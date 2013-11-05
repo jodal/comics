741e26cd731e1e31b299b6f623c89a32b7531b6f
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-11-05 05:56:34 +0100

    Update 'bugcomics' name

--- comics/comics/bugcomic.py
+++ comics/comics/bugcomic.py
@@ -3,7 +3,7 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Bug'
+    name = 'Bug Martini'
     language = 'en'
     url = 'http://www.bugmartini.com/'
     start_date = '2009-10-19'
