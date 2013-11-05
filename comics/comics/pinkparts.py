f7f57015d57eac433d8e785ff4c6afad2eb04417
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-11-05 06:53:08 +0100

    Fix indentation in 'pinkparts' and 'poledancingadventures'

--- comics/comics/pinkparts.py
+++ comics/comics/pinkparts.py
@@ -6,19 +6,19 @@ class ComicData(ComicDataBase):
     name = 'Pink Parts'
     language = 'en'
     url = 'http://pinkpartscomic.com/'
-	start_date = '2010-02-01'
+    start_date = '2010-02-01'
     rights = 'Katherine Skipper'
 
 
 class Crawler(CrawlerBase):
-	history_capable_date = '2010-02-01'
-	schedule = None
+    history_capable_date = '2010-02-01'
+    schedule = None
     time_zone = 'US/Pacific'
 
     def crawl(self, pub_date):
         feed = self.parse_feed('http://pinkpartscomic.com/inc/feed.php')
         for entry in feed.for_date(pub_date):
-			url = entry.sunnary.src('img[src*="/comics/"]')
-			title = entry.title
-			text = entry.summary.alt('img[src*="/comics/"]')
-			return CrawlerImage(url, title, text)
+            url = entry.sunnary.src('img[src*="/comics/"]')
+            title = entry.title
+            text = entry.summary.alt('img[src*="/comics/"]')
+            return CrawlerImage(url, title, text)
--- comics/comics/poledancingadventures.py
+++ comics/comics/poledancingadventures.py
@@ -6,19 +6,18 @@ class ComicData(ComicDataBase):
     name = 'Pole Dancing Adventures'
     language = 'en'
     url = 'http://pole-dancing-adventures.blogspot.com/'
-	start_date = '2010-01-28'
+    start_date = '2010-01-28'
     rights = 'Leen Isabel'
 
 
 class Crawler(CrawlerBase):
-	history_capable_date = '2010-01-28'
-	schedule = None
+    history_capable_date = '2010-01-28'
     time_zone = 'US/Pacific'
 
     def crawl(self, pub_date):
         feed = self.parse_feed('http://feed.feedburner.com/blogspot/zumUM')
         for entry in feed.for_date(pub_date):
-			url = entry.sunnary.src('img[src*="/comics/"]')
-			title = entry.title
-			text = entry.summary.alt('img[src*="/comics/"]')
-			return CrawlerImage(url, title, text)
+            url = entry.sunnary.src('img[src*="/comics/"]')
+            title = entry.title
+            text = entry.summary.alt('img[src*="/comics/"]')
+            return CrawlerImage(url, title, text)
