9f6df6ab7b208158c6dd7c81df8542a5e354e408
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-03-31 23:06:02 +0200

    Django 1.6: Enable XFrameOptionsMiddleware

--- comics/settings/base.py
+++ comics/settings/base.py
@@ -68,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
     'comics.core.middleware.MinifyHTMLMiddleware',
 )
