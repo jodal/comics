7ca8f80656c56d280b8ca44320e71e06f2bca294
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-04-01 00:06:21 +0200

    Django 1.6: Change Manager.{get_query_set => get_queryset}()

--- comics/core/managers.py
+++ comics/core/managers.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ComicManager(models.Manager):
     def sort_by_name(self):
-        qs = self.get_query_set()
+        qs = self.get_queryset()
         qs = qs.extra(select={'lower_name': 'LOWER(name)'})
         qs = qs.extra(order_by=['lower_name'])
         return qs
