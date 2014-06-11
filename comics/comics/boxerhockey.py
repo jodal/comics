4dc3fc1528c1554dd1c7e76d96026ec353a83dc3
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-06-11 10:14:03 +0200

    Mark 'boxerhockey' as inactive

--- comics/comics/boxerhockey.py
+++ comics/comics/boxerhockey.py
@@ -7,18 +7,11 @@ class ComicData(ComicDataBase):
     language = 'en'
     url = 'http://boxerhockey.fireball20xl.com/'
     start_date = '2007-11-25'
+    end_date = '2013-08-03'
+    active = False
     rights = 'Tyson "Rittz" Hesse'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 120
-    time_zone = 'US/Pacific'
-
     def crawl(self, pub_date):
-        feed = self.parse_feed(
-            'http://boxerhockey.fireball20xl.com/inc/feed.php')
-        for entry in feed.for_date(pub_date):
-            title = entry.title
-            page = self.parse_page(entry.id)
-            url = page.src('img#comicimg')
-            return CrawlerImage(url, title)
+        pass  # Comic no longer published
