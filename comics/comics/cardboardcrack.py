36123672050252b4f1dec50477f09438d01ea7ab
jimtrim <jimfrodehoff@gmail.com>
2014-07-18 05:26:33 +0200

    add crawler for Cardboard Crack

---
+++ comics/comics/cardboardcrack.py
@@ -0,0 +1,21 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+
+class ComicData(ComicDataBase):
+    name = 'Cardboard Crack'
+    language = 'en'
+    url = 'http://cardboard-crack.com/'
+    start_date = '2013-03-01'
+
+
+class Crawler(CrawlerBase):
+    history_capable_days = 30
+    schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
+    time_zone = 'US/Pacific'
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed('http://cardboard-crack.com/rss')
+        for entry in feed.for_date(pub_date):
+            url = entry.summary.src('img[src$="500.gif"]')
+            return CrawlerImage(url)
