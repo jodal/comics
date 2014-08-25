e57df2d190787696c7edab96ce3336740196c11f
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-08-25 23:36:04 +0200

    Update 'seemikedraw' after site move

--- comics/comics/seemikedraw.py
+++ comics/comics/seemikedraw.py
@@ -5,7 +5,7 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = 'seemikedraw'
     language = 'en'
-    url = 'http://seemikedraw.com.au/'
+    url = 'http://mikejacobsen.tumblr.com/'
     start_date = '2007-07-31'
     rights = 'Mike Jacobsen'
 
@@ -14,12 +14,9 @@ class Crawler(CrawlerBase):
     history_capable_days = 180
     time_zone = 'Australia/Sydney'
 
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {'User-Agent': 'Mozilla/4.0'}
-
     def crawl(self, pub_date):
-        feed = self.parse_feed('http://seemikedraw.com.au/feed')
+        feed = self.parse_feed('http://mikejacobsen.tumblr.com/rss')
         for entry in feed.for_date(pub_date):
-            url = entry.content0.src('img[src*="/wp-content/uploads/"]')
+            url = entry.summary.src('img')
             title = entry.title
             return CrawlerImage(url, title)
