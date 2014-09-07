2d9748b49461f8fd56e49f0a7652bead0405f78c
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-09-07 18:42:13 +0200

    Add crawler for 'lunche24'

---
+++ comics/comics/lunche24.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+
+class ComicData(ComicDataBase):
+    name = 'Lunch (e24.no)'
+    language = 'no'
+    url = 'http://www.e24.no/lunch/'
+    start_date = '2009-10-21'
+    rights = 'Børge Lund'
+
+
+class Crawler(CrawlerBase):
+    history_capable_date = '2012-11-02'
+    schedule = 'Mo,Tu,We,Th,Fr,Sa'
+    time_zone = 'Europe/Oslo'
+
+    def crawl(self, pub_date):
+        url = 'http://static.e24.no/images/comics/lunch_%s.gif' % (
+            pub_date.strftime('%Y%m%d'))
+        return CrawlerImage(url)
--- docs/changes.rst
+++ docs/changes.rst
@@ -21,6 +21,7 @@ v2.3.2 (UNRELEASED)
 
 - New: ``blasternation``
 - New: ``cardboardcrack``
+- New: ``lunche24``
 - Update: ``asofterworld`` after site change.
 - Update: ``doghouse`` after feed change.
 - Update: ``joyoftech`` after site change.
