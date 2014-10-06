2f3bd3210b27c482561faf4f1b3ccbf76fc15a94
xim <xim-git@akuma.no>
2014-10-06 09:55:23 +0200

    Subnormality crawler update: Sort by style tag contents.
    This is now quite possibly the ugliest crawler.

--- comics/comics/subnormality.py
+++ comics/comics/subnormality.py
@@ -1,6 +1,8 @@
 from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
+import re
+
 
 class ComicData(ComicDataBase):
     name = 'Subnormality'
@@ -18,7 +20,12 @@ class Crawler(CrawlerBase):
         feed = self.parse_feed('http://www.viruscomix.com/rss.xml')
         for entry in feed.for_date(pub_date):
             page = self.parse_page(entry.link)
-            url = page.src('body > img[src$=".jpg"]')
-            title = page.text('title')
-            text = page.title('body > img[src$=".jpg"]')
-            return CrawlerImage(url, title, text)
+            els = filter(lambda el: el.attrib['src'].endswith('.jpg'),
+                    page.root.find('body').findall('img'))
+            els.sort(key=lambda el: int(
+                re.match(r'.*top:\s*(\d+).*', el.attrib['style']).group(1)))
+            result = [CrawlerImage(el.attrib['src'], None, el.attrib.get('title', None))
+                    for el in els]
+            if result:
+                result[0].title = page.text('title')
+            return result
