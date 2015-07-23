9f965227bc02509db9531300af68f4568e00277e
datagutten <datagutten@datagutten.net>
2015-07-23 12:44:37 +0200

    New history-capable source for 'beetlebailey'

--- comics/comics/beetlebailey.py
+++ comics/comics/beetlebailey.py
@@ -1,19 +1,25 @@
-from comics.aggregator.crawler import ArcaMaxCrawlerBase
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = 'Beetle Bailey'
     language = 'en'
-    url = 'http://www.arcamax.com/thefunnies/beetlebailey/'
+    url = 'http://beetlebailey.com'
     start_date = '1950-01-01'
     rights = 'Mort Walker'
 
 
-class Crawler(ArcaMaxCrawlerBase):
-    history_capable_days = 0
+class Crawler(CrawlerBase):
+    history_capable_date = '1996-07-07'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'US/Eastern'
 
     def crawl(self, pub_date):
-        return self.crawl_helper('beetlebailey', pub_date)
+        date = pub_date.strftime('%B-')
+        date += pub_date.strftime('%d').lstrip('0')
+        date += pub_date.strftime('-%Y')
+        page_url = 'http://beetlebailey.com/comics/' + date
+        page = self.parse_page(page_url)
+        url = page.src('img[src*="safr.kingfeatures.com"]')
+        return CrawlerImage(url)
