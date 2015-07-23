eebbcb3835a12ae854434b537225a3090ca116f1
datagutten <datagutten@datagutten.net>
2015-07-23 10:20:14 +0200

    New history-capable source for 'babyblues'

--- comics/comics/babyblues.py
+++ comics/comics/babyblues.py
@@ -5,17 +5,21 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = 'Baby Blues'
     language = 'en'
-    url = 'http://www.arcamax.com/babyblues'
+    url = 'http://www.babyblues.com'
     start_date = '1990-01-01'
     rights = 'Rick Kirkman and Jerry Scott'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 0
+    history_capable_date = '1995-01-08'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'US/Eastern'
 
     def crawl(self, pub_date):
-        page = self.parse_page('http://www.arcamax.com/babyblues')
-        url = page.src('img[id^="comic-zoom"]')
+        date = pub_date.strftime('%B-')
+        date += pub_date.strftime('%d').lstrip('0')
+        date += pub_date.strftime('-%Y')
+        page_url = 'http://babyblues.com/comics/' + date
+        page = self.parse_page(page_url)
+        url = page.src('img[src*="safr.kingfeatures.com"]')
         return CrawlerImage(url)
