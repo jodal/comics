0afba1b30b1e52fd2162b2ffbb7e2513eaa3399a
datagutten <datagutten@datagutten.net>
2015-07-23 10:42:22 +0200

    New history-capable source for 'dustin'

--- comics/comics/dustin.py
+++ comics/comics/dustin.py
@@ -1,19 +1,25 @@
-from comics.aggregator.crawler import ArcaMaxCrawlerBase
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = 'Dustin'
     language = 'en'
-    url = 'http://www.arcamax.com/thefunnies/dustin/'
+    url = 'http://dustincomics.com'
     start_date = '2010-01-04'
     rights = 'Steve Kelley & Jeff Parker'
 
 
-class Crawler(ArcaMaxCrawlerBase):
-    history_capable_days = 0
+class Crawler(CrawlerBase):
+    history_capable_date = '2010-01-04'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'US/Eastern'
 
     def crawl(self, pub_date):
-        return self.crawl_helper('dustin', pub_date)
+        date = pub_date.strftime('%B-')
+        date += pub_date.strftime('%d').lstrip('0')
+        date += pub_date.strftime('-%Y')
+        page_url = 'http://dustincomics.com/comics/' + date
+        page = self.parse_page(page_url)
+        url = page.src('img[src*="safr.kingfeatures.com"]')
+        return CrawlerImage(url)
