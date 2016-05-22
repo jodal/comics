824d33e80432644f7b6ce283b0cb166f5f61fff7
Vidar Løkken <vidarlo@bitsex.net>
2016-05-22 15:38:12 +0200

    Rutetid appears to be active again

--- comics/comics/rutetid.py
+++ comics/comics/rutetid.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from comics.aggregator.crawler import CrawlerBase
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,10 +8,16 @@ class ComicData(ComicDataBase):
     name = 'Rutetid'
     language = 'no'
     url = 'http://www.dagbladet.no/tegneserie/rutetid/'
-    active = False
     rights = 'Frode Øverli'
 
 
 class Crawler(CrawlerBase):
+    history_capable_days = 10
+    schedule = 'Mo,Tu,We,Th,Fr'
+    time_zone = 'Europe/Oslo'
+
     def crawl(self, pub_date):
-        pass  # Comic no longer published
+        epoch = self.date_to_epoch(pub_date)
+        url = 'http://www.dagbladet.no/tegneserie/pondusarkiv/serveconfig.php?strip=rutetid&date=%s' % (
+            epoch,)
+        return CrawlerImage(url)
