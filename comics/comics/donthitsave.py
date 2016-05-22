061e7e71f929a2e7acefcbff967bd8cf1831c4ef
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-05-22 13:55:02 +0200

    Actually add 'donthitsave' crawler

---
+++ comics/comics/donthitsave.py
@@ -0,0 +1,24 @@
+from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.core.comic_data import ComicDataBase
+
+
+class ComicData(ComicDataBase):
+    name = "Don't Hit Save"
+    language = 'en'
+    url = 'http://www.donthitsave.com/'
+    start_date = '2015-09-18'
+    rights = 'Jeff Lofvers'
+
+
+class Crawler(CrawlerBase):
+    history_capable_days = 60
+    schedule = 'Tu,Fr'
+    time_zone = 'US/Eastern'
+
+    def crawl(self, pub_date):
+        feed = self.parse_feed(
+            'http://www.donthitsave.com/donthitsavefeed.xml')
+        for entry in feed.for_date(pub_date):
+            url = entry.summary.src('img')
+            title = entry.title
+            return CrawlerImage(url, title)
