ac75bb6d83f8a9244630aa28e4e78fb1f55647a3
Stein Magnus Jodal <stein.magnus@jodal.no>
2017-04-02 11:45:47 +0200

    Remove Python 2.6 compat

--- comics/__init__.py
+++ comics/__init__.py
@@ -2,7 +2,4 @@ import logging
 
 # Install a default log handler so that we don't get errors about missing log
 # handlers when running tests.
-#
-# logging.NullHandler is new in Python 2.7, so we do this conditionally.
-if hasattr(logging, 'NullHandler'):
-    logging.getLogger('comics').addHandler(logging.NullHandler())
+logging.getLogger('comics').addHandler(logging.NullHandler())
