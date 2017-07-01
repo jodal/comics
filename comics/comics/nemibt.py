c2627ce96e42b4f7db55493732b3149bdeb65cf9
datagutten <datagutten@datagutten.net>
2017-07-01 09:40:14 +0200

    Update 'nemibt' after site change

--- comics/comics/nemibt.py
+++ comics/comics/nemibt.py
@@ -5,17 +5,17 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = 'Nemi (bt.no)'
     language = 'no'
-    url = 'http://www.bt.no/bergenpuls/tegneserier/tegneserier_nemi/'
+    url = 'https://www.bt.no/kultur/tegneserier/'
     start_date = '1997-01-01'
     rights = 'Lise Myhre'
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 162
+    history_capable_date = '2008-07-01'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'Europe/Oslo'
 
     def crawl(self, pub_date):
-        url = 'http://www.bt.no/external/cartoon/nemi/%s.gif' % (
+        url = 'https://cartoon-prod.schibsted.tech/nemi/%s.gif' % (
             pub_date.strftime('%d%m%y'),)
         return CrawlerImage(url)
