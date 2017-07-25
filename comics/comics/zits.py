511c34211bd6c7f181049497293ed27b988224c4
datagutten <datagutten@datagutten.net>
2017-07-25 13:09:42 +0200

    Updated 'zits' to use KingFeaturesCrawlerBase

--- comics/comics/zits.py
+++ comics/comics/zits.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import KingFeaturesCrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -10,16 +10,10 @@ class ComicData(ComicDataBase):
     rights = 'Jerry Scott and Jim Borgman'
 
 
-class Crawler(CrawlerBase):
+class Crawler(KingFeaturesCrawlerBase):
     history_capable_date = '2005-01-01'
     schedule = 'Mo,Tu,We,Th,Fr,Sa,Su'
     time_zone = 'US/Eastern'
 
     def crawl(self, pub_date):
-        date = pub_date.strftime('%B-')
-        date += pub_date.strftime('%d').lstrip('0')
-        date += pub_date.strftime('-%Y')
-        page_url = 'http://zitscomics.com/comics/' + date
-        page = self.parse_page(page_url)
-        url = page.src('img[src*="safr.kingfeatures.com"]')
-        return CrawlerImage(url)
+        return self.crawl_helper('zitscomics.com', pub_date)
