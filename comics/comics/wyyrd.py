3c0438faeb96fd0cb21044660ac5ff4966df8c74
datagutten <datagutten@datagutten.net>
2017-07-25 13:05:15 +0200

    Update 'wyyrd' after site change

--- comics/comics/wyyrd.py
+++ comics/comics/wyyrd.py
@@ -1,4 +1,5 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+# encoding: utf-8
+from comics.aggregator.crawler import NettserierCrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -10,15 +11,9 @@ class ComicData(ComicDataBase):
     rights = 'Gard Robot Helset'
 
 
-class Crawler(CrawlerBase):
+class Crawler(NettserierCrawlerBase):
     history_capable_date = '2008-01-14'
     time_zone = 'Europe/Oslo'
 
     def crawl(self, pub_date):
-        url = 'http://wyyrd.nettserier.no/_striper/wyyrd-%s.png' % (
-            self.date_to_epoch(pub_date),)
-        page_url = 'http://wyyrd.nettserier.no/%s' % (
-            pub_date.strftime('%Y/%m/%d'))
-        page = self.parse_page(page_url)
-        title = page.alt('img[src*="/_striper/"]')
-        return CrawlerImage(url, title)
+        return self.crawl_helper('wyyrd', pub_date)
