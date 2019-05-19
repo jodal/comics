6391a3f04ca87a7e75779e5481029d2e265951fe
datagutten <datagutten@datagutten.net>
2019-05-19 21:03:09 +0200

    Dagbladet no longer publishes lunch and pondus

--- comics/comics/pondus.py
+++ comics/comics/pondus.py
@@ -9,13 +9,10 @@ class ComicData(ComicDataBase):
     language = 'no'
     url = 'http://www.dagbladet.no/tegneserie/pondus/'
     start_date = '1995-01-01'
+    active = False
     rights = 'Frode Øverli'
 
 
 class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 14
-    schedule = 'Mo,Tu,We,Th,Fr,Sa'
-    time_zone = 'Europe/Oslo'
-
     def crawl(self, pub_date):
-        return self.crawl_helper('pondus', pub_date)
+        pass  # Comic no longer published on this site
