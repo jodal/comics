b090d08d4b9b8d24d070e9cd97ba85186488a938
datagutten <datagutten@datagutten.net>
2019-05-25 21:41:36 +0200

    Rocky is no longer published on bt.no

--- comics/comics/rockybt.py
+++ comics/comics/rockybt.py
@@ -5,14 +5,9 @@ from comics.comics.rocky import ComicData as RockyData
 class ComicData(RockyData):
     name = 'Rocky (bt.no)'
     url = 'https://www.bt.no/kultur/tegneserier/'
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = '2008-07-01'
-    schedule = 'Mo,Tu,We,Th,Fr,Sa'
-    time_zone = 'Europe/Oslo'
-
     def crawl(self, pub_date):
-        url = 'https://cartoon-prod.schibsted.tech/rocky/%s.gif' % (
-            pub_date.strftime('%d%m%y'),)
-        return CrawlerImage(url)
+        pass
