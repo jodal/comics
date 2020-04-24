f522128dfc01022710ebfb24211fb09ca19c4cc8
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-04-25 00:43:18 +0200

    Adjust nemi schedule

--- comics/comics/nemi.py
+++ comics/comics/nemi.py
@@ -12,7 +12,7 @@ class ComicData(ComicDataBase):
 
 class Crawler(DagbladetCrawlerBase):
     history_capable_days = 30
-    schedule = 'Mo,Tu,We,Th,Fr,Sa'
+    schedule = 'Sa'
     time_zone = 'Europe/Oslo'
 
     def crawl(self, pub_date):
