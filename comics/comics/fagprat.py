c69791760665db0739196729fe0533e679be2ea9
datagutten <datagutten@datagutten.net>
2020-05-16 09:30:56 +0200

    "Fagprat" is no longer published on dagbladet.no

--- comics/comics/fagprat.py
+++ comics/comics/fagprat.py
@@ -9,11 +9,10 @@ class ComicData(ComicDataBase):
     language = "no"
     url = "http://www.dagbladet.no/tegneserie/fagprat"
     rights = "Flu Hartberg"
+    active = False
 
 
 class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 14
-    schedule = "Tu,Th,Sa"
     time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
