3f37ef4ca5d4f8b098de4e7a170b05f1ed1f8252
datagutten <datagutten@datagutten.net>
2020-05-16 15:03:16 +0200

    Rewrite crawler for "Fowl Language" after feed change and add history capability

--- comics/comics/fowllanguage.py
+++ comics/comics/fowllanguage.py
@@ -11,16 +11,41 @@ class ComicData(ComicDataBase):
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 10
-    schedule = "Mo,We,Fr"
+    history_capable_date = "2013-07-22"
     time_zone = "US/Eastern"
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.fowllanguagecomics.com/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src("img.wp-post-image")
-            if url is None:
-                continue
-            url = url.replace("?resize=150%2C150", "")
-            title = entry.title
-            return CrawlerImage(url, title)
+        page_url = "https://www.fowllanguagecomics.com/archive"
+        archive_page = self.parse_page(page_url)
+        release = archive_page.root.xpath(
+            '//p[(@class="ar-extra-text") and (.="%s")]/..'
+            % pub_date.strftime("%b %d, %Y")
+        )
+        if not release:
+            return
+        release = release[0]
+        title = release.xpath('p[@class="ar-text"]')
+        title = title[0].text
+
+        url = release.xpath("img/@data-lazy-src")
+        url = url[0].replace("?fit=200%2C250&ssl=1", "")
+
+        link = release.xpath('a[@class="ar-btn"]/@href')
+        link = link[0]
+
+        release_page = self.parse_page(link)
+        bonus_link = release_page.root.xpath('//a[@class="bonus-btn"]/@href')
+        if bonus_link:
+            bonus_link = bonus_link[0]
+            bonus_page = self.parse_page(bonus_link)
+            bonus_url = bonus_page.root.xpath("//img/@data-lazy-src")
+            bonus_url = bonus_url[0].replace("?fit=800%2C510&#038;ssl=1", "")
+            bonus_title = bonus_page.root.xpath('//h2[@class="post-title"]')
+            bonus_title = bonus_title[0].text
+            return [
+                CrawlerImage(url=url, title=title),
+                CrawlerImage(url=bonus_url, title=bonus_title),
+            ]
+        else:
+            return CrawlerImage(url=url, title=title)
