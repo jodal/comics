190e639af407e11152603222d3bfa5799ef94920
datagutten <datagutten@datagutten.net>
2020-05-17 21:47:04 +0200

    "Fatawesome" is no longer active and feed is gone

--- comics/comics/fatawesome.py
+++ comics/comics/fatawesome.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,18 +8,9 @@ class ComicData(ComicDataBase):
     url = "http://www.fatawesome.com/"
     start_date = "2014-09-16"
     rights = "James Craig"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2014-09-16"
-    time_zone = "US/Eastern"
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.fatawesome.com/feed/")
-        for entry in feed.for_date(pub_date):
-            if "Comics" not in entry.tags:
-                continue
-            url = entry.content0.src("img")
-            title = entry.title
-            text = entry.content0.alt("img")
-            return CrawlerImage(url, title, text)
+        pass
