1df7544d09232357082fdd84828be45bf6fed136
datagutten <datagutten@datagutten.net>
2020-05-21 12:02:28 +0200

    "Not Invented Here" is no longer active

--- comics/comics/notinventedhere.py
+++ comics/comics/notinventedhere.py
@@ -8,11 +8,11 @@ class ComicData(ComicDataBase):
     url = "http://notinventedhe.re/"
     start_date = "2009-09-21"
     rights = "Bill Barnes and Paul Southworth"
+    active = False
 
 
 class Crawler(CrawlerBase):
     history_capable_date = "2009-09-21"
-    schedule = "Tu,Th"
     time_zone = "US/Pacific"
 
     def crawl(self, pub_date):
