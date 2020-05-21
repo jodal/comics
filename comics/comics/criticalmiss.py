6fac97f5f0de46de588e4829a1e9fb9782b72597
datagutten <datagutten@datagutten.net>
2020-05-21 21:06:30 +0200

    "Critical miss" is no longer active and feed is gone

--- comics/comics/criticalmiss.py
+++ comics/comics/criticalmiss.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -11,22 +11,11 @@ class ComicData(ComicDataBase):
     )
     start_date = "2010-05-18"
     rights = "Cory Rydell & Grey Carter"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 200
-    schedule = "Tu,Fr"
     time_zone = "US/Pacific"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed(
-            "http://rss.escapistmagazine.com"
-            "/articles/comicsandcosplay/comics/critical-miss"
-        )
-        for entry in feed.for_date(pub_date):
-            page = self.parse_page(entry.link)
-            url = page.src('.body img[src$=".png"]')
-            title = entry.title
-            if title is not None:
-                title = title.replace("Critical Miss: ", "")
-            return CrawlerImage(url, title)
+        pass
