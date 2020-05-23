d677f3762e0daf16e1be05a88b058194ddf43e15
datagutten <datagutten@datagutten.net>
2020-05-23 09:51:29 +0200

    Rewrite "The Perry Bible Fellowship" after feed change

--- comics/comics/perrybiblefellowship.py
+++ comics/comics/perrybiblefellowship.py
@@ -11,12 +11,15 @@ class ComicData(ComicDataBase):
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2001-01-01"
+    history_capable_date = "2019-06-12"
     time_zone = "US/Eastern"
 
     def crawl(self, pub_date):
         feed = self.parse_feed("http://www.pbfcomics.com/feed/feed.xml")
         for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/archive_b/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+            page = self.parse_page(entry.link)
+            images = page.root.xpath("//div[@id='comic']/img")
+            crawler_images = []
+            for image in images:
+                title = entry.title
+                crawler_images.append(CrawlerImage(image.get("src"), title))
