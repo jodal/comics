57e83ad400cc949d47aa4e80da3fee90aec865fb
datagutten <datagutten@datagutten.net>
2022-05-18 17:57:19 +0200

    "Lunch" is no longer published on e24.no

--- comics/comics/lunche24.py
+++ comics/comics/lunche24.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,15 +8,11 @@ class ComicData(ComicDataBase):
     url = "http://www.e24.no/lunch/"
     start_date = "2009-10-21"
     rights = "Børge Lund"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2012-11-02"
-    schedule = "Mo,Tu,We,Th,Fr,Sa"
     time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
-        url = "http://static.e24.no/images/comics/lunch_%s.gif" % (
-            pub_date.strftime("%Y%m%d")
-        )
-        return CrawlerImage(url)
+        pass
