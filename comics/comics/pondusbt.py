93116c026b86c6996972ee85197ab1d391edab11
datagutten <datagutten@datagutten.net>
2022-05-18 18:14:02 +0200

    "Nemi" and "Pondus" are no longer published on bt.no

--- comics/comics/pondusbt.py
+++ comics/comics/pondusbt.py
@@ -5,15 +5,11 @@ from comics.comics.pondus import ComicData as PondusData
 class ComicData(PondusData):
     name = "Pondus (bt.no)"
     url = "https://www.bt.no/kultur/tegneserier/"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2008-07-01"
-    schedule = "Mo,Tu,We,Th,Fr,Sa"
     time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
-        url = "https://cartoon-prod.schibsted.tech/pondus/{}.gif".format(
-            pub_date.strftime("%d%m%y"),
-        )
-        return CrawlerImage(url)
+        pass
