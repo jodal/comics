af5423b47bb56eb2799e096b8235ede07ea9f266
datagutten <datagutten@datagutten.net>
2024-12-23 16:54:13 +0100

    Reformat with ruff

--- comics/comics/anleggsplassen.py
+++ comics/comics/anleggsplassen.py
@@ -16,10 +16,11 @@ class Crawler(CrawlerBase):
     schedule = "Fr"
     time_zone = "Europe/Oslo"
     headers = {
-        "User-Agent":
+        "User-Agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/131.0.0.0 Safari/537.36",
+            "Chrome/131.0.0.0 Safari/537.36"
+        ),
     }
 
     def crawl(self, pub_date):
