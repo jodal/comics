b6cb1412eb309bf1ab16b9bfb548be787f5f83bb
Stein Magnus Jodal <stein.magnus@jodal.no>
2024-12-26 00:31:26 +0100

    Make a console script entrypoint

--- src/comics/__main__.py
+++ src/comics/__main__.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "comics.settings")
 
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "comics.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
