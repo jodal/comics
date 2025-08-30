2e67973e2408117fe6e29d40f0c192885e818c60
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 00:52:29 +0200

    crawlers: Remove crawler for inactive comic bestis

--- src/comics/comics/bestis.py
+++ src/comics/comics/bestis.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import DagbladetCrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,12 +7,3 @@ class ComicData(ComicDataBase):
     url = "https://www.dagbladet.no/tegneserie/bestis/"
     rights = "Kenneth Larsen"
     active = False
-
-
-class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 30
-    schedule = "Sa"
-    time_zone = "Europe/Oslo"
-
-    def crawl(self, pub_date):
-        return self.crawl_helper("bestis", pub_date)
