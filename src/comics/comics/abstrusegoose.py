85a097c7d1da8724877f2b43fdaa7f24287df689
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 19:23:45 +0200

    crawlers: Mark abstrusegoose as inactive

--- src/comics/comics/abstrusegoose.py
+++ src/comics/comics/abstrusegoose.py
@@ -1,24 +1,15 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = "Abstruse Goose"
     language = "en"
-    url = "http://www.abstrusegoose.com/"
     start_date = "2008-02-01"
     rights = "lcfr, CC BY-NC 3.0 US"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 10
-    schedule = "Mo,Th"
-    time_zone = "America/Los_Angeles"
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://abstrusegoose.com/atomfeed.xml")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/strips/"]')
-            title = entry.title
-            text = entry.summary.title('img[src*="/strips/"]')
-            return CrawlerImage(url, title, text)
+        pass  # Comic no longer published
