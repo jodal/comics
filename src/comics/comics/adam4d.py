de6ce163b385c34a97d0183eb6e34fa17348e758
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 19:25:10 +0200

    crawlers: Mark adam4d as inactive

--- src/comics/comics/adam4d.py
+++ src/comics/comics/adam4d.py
@@ -1,28 +1,15 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = "Adam4d.com"
     language = "en"
-    url = "http://www.adam4d.com/"
     start_date = "2012-07-03"
     rights = "Adam Ford"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 10
-    time_zone = "America/New_York"
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://adam4d.com/feed/")
-        for entry in feed.for_date(pub_date):
-            urls = [
-                url.replace("comics-rss", "comics")
-                for url in entry.content0.srcs('img[src*="/wp-content/"]')
-            ]
-            results = [CrawlerImage(url) for url in urls]
-            if not results:
-                continue
-            results[0].title = entry.title
-            return results
+        pass  # Comic no longer published
