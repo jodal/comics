5abd087ae5a615e7bb0aa68508859fbe3dce0992
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 01:36:11 +0200

    crawlers: Mark schlockmercenary as inactive

--- src/comics/comics/schlockmercenary.py
+++ src/comics/comics/schlockmercenary.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,14 +7,4 @@ class ComicData(ComicDataBase):
     url = "https://www.schlockmercenary.com/"
     start_date = "2000-06-12"
     rights = "Howard Tayler"
-
-
-class Crawler(CrawlerBase):
-    history_capable_date = "2000-06-12"
-    schedule = "Mo,Tu,We,Th,Fr,Sa,Su"
-    time_zone = "America/Denver"
-
-    def crawl(self, pub_date):
-        page_url = "https://www.schlockmercenary.com/%s" % pub_date.strftime("%Y-%m-%d")
-        page = self.parse_page(page_url)
-        return [CrawlerImage(url) for url in page.srcs("#comic img")]
+    active = False
