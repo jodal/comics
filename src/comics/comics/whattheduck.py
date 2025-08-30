6e16d2546ccb5b51efc42351e0522f0472c0492b
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 01:34:21 +0200

    crawlers: Mark whattheduck as inactive

--- src/comics/comics/whattheduck.py
+++ src/comics/comics/whattheduck.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,18 +7,4 @@ class ComicData(ComicDataBase):
     url = "http://www.whattheduck.net/"
     start_date = "2006-07-01"
     rights = "Aaron Johnson"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 7
-    time_zone = "America/Chicago"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.whattheduck.net/strip/rss.xml")
-        for entry in feed.for_date(pub_date):
-            if entry.enclosures[0].type.startswith("image") and entry.title.startswith(
-                "WTD"
-            ):
-                url = entry.enclosures[0].href
-                title = entry.title
-                return CrawlerImage(url, title)
+    active = False
