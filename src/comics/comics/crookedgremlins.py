c831aa877c56feea030525e5451a0a10a2b77105
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 18:50:20 +0200

    crawlers: Mark crookedgremlins as inactive

--- src/comics/comics/crookedgremlins.py
+++ src/comics/comics/crookedgremlins.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,22 +7,11 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.crookedgremlins.com/"
     start_date = "2008-04-01"
+    end_date = "2015-07-28"
     rights = "Carter Fort and Paul Lucci"
     active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2008-04-01"
-    time_zone = "America/Los_Angeles"
-
     def crawl(self, pub_date):
-        page = self.parse_page(
-            "http://crookedgremlins.com/%s/" % pub_date.strftime("%Y/%m/%d")
-        )
-        title = page.alt("#comic img")
-        url = page.src("#comic img")
-
-        # Put together the text from multiple paragraphs
-        text = "\n\n".join(page.texts(".post-content p")).strip()
-
-        return CrawlerImage(url, title, text)
+        pass  # Comic no longer published
