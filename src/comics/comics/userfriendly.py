246413802d6a5a241b0c575049056f71e97dbf17
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 18:37:03 +0200

    crawlers: Mark userfriendly as inactive

--- src/comics/comics/userfriendly.py
+++ src/comics/comics/userfriendly.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,21 +7,11 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.userfriendly.org/"
     start_date = "1997-11-17"
+    end_date = "2010-11-21"
     rights = 'J.D. "Illiad" Frazer'
+    active = False
 
 
 class Crawler(CrawlerBase):
-    has_rerun_releases = True
-    history_capable_date = "1997-11-17"
-    schedule = "Mo,Tu,We,Th,Fr,Sa,Su"
-    time_zone = "America/Vancouver"
-
     def crawl(self, pub_date):
-        page_url = "http://ars.userfriendly.org/cartoons/?id={}".format(
-            pub_date.strftime("%Y%m%d"),
-        )
-        page = self.parse_page(page_url)
-        url = page.src('img[alt^="Strip for"]')
-        if not url:  # Old releases
-            url = page.src('img[src^="http://www.userfriendly.org/cartoons/archives/"]')
-        return CrawlerImage(url)
+        pass  # Comic no longer published
