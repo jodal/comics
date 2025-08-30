2b2a994f2cd7b042d3f3f84bae95e5c618c88bd3
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 19:26:02 +0200

    crawlers: Mark amazingsuperpowers as inactive

--- src/comics/comics/amazingsuperpowers.py
+++ src/comics/comics/amazingsuperpowers.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,20 +8,9 @@ class ComicData(ComicDataBase):
     url = "http://www.amazingsuperpowers.com/"
     start_date = "2007-09-24"
     rights = "Wes & Tony"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 30
-    schedule = "Mo,Th"
-    time_zone = "America/New_York"
-
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {"User-Agent": "Mozilla/4.0"}
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/amazingsuperpowers")
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src('img[src*="/comics/"]')
-            title = entry.title
-            text = entry.content0.title('img[src*="/comics/"]')
-            return CrawlerImage(url, title, text)
+        pass  # Comic no longer published
