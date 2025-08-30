15b257ef95d9e133a5c6dd30e434d1047ef8caef
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 18:48:46 +0200

    crawlers: Mark crfh as inactive

--- src/comics/comics/crfh.py
+++ src/comics/comics/crfh.py
@@ -1,13 +1,15 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = "Colleges Roomies from Hell"
     language = "en"
-    url = "http://www.crfh.net/"
+    url = "https://www.maritzacampos.com/crfh/"
     start_date = "1999-01-01"
+    end_date = "2025-02-24"
     rights = "Maritza Campos"
+    active = False
 
 
 class Crawler(CrawlerBase):
@@ -15,10 +17,4 @@ class Crawler(CrawlerBase):
     time_zone = "America/Merida"
 
     def crawl(self, pub_date):
-        page_url = "http://www.crfh.net/d/{}.html".format(pub_date.strftime("%Y%m%d"))
-        page = self.parse_page(page_url)
-        url = page.src('img[src*="crfh%s"]' % pub_date.strftime("%Y%m%d"))
-        if url is None:
-            return
-        url = url.replace("\n", "")
-        return CrawlerImage(url)
+        pass  # Comic no longer published
