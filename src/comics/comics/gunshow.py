a69182c209cc71787deb9a34e17c6b290f4db575
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 18:30:29 +0200

    crawlers: Mark gunshow as inactive

--- src/comics/comics/gunshow.py
+++ src/comics/comics/gunshow.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,19 +7,11 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "https://www.gunshowcomic.com/"
     start_date = "2008-09-04"
+    end_date = "2015-01-01"
     rights = '"Lord KC Green"'
     active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2008-09-04"
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
     def crawl(self, pub_date):
-        page_url = "https://www.gunshowcomic.com/d/{}.html".format(
-            pub_date.strftime("%Y%m%d"),
-        )
-        page = self.parse_page(page_url)
-        urls = page.srcs('img[src^="http://gunshowcomic.com/comics/"]')
-        return [CrawlerImage(url) for url in urls]
+        pass  # Comic no longer published
