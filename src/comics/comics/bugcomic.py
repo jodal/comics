48a602fbaeb43cd7bde0f77f97a8f76004f73e12
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-30 19:27:14 +0200

    crawlers: Mark bugcomic as inactive

--- src/comics/comics/bugcomic.py
+++ src/comics/comics/bugcomic.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,18 +8,9 @@ class ComicData(ComicDataBase):
     url = "http://www.bugmartini.com/"
     start_date = "2009-10-19"
     rights = "Adam Huber"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 15
-    schedule = "Mo,Tu,We,Th,Fr"
-    time_zone = "America/Denver"
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.bugmartini.com/feed/")
-        for entry in feed.for_date(pub_date):
-            title = entry.title
-            url = entry.summary.src('img[src*="/wp-content/uploads/"]')
-            if url:
-                url = url.replace("?resize=520%2C280", "")
-            return CrawlerImage(url, title)
+        pass  # Comic no longer published
