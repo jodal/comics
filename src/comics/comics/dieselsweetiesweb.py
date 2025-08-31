6255ad02e44bbb5b798c2009931cbee022bac230
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:49:50 +0200

    crawlers: Mark dieselsweetiesweb as inactive

--- src/comics/comics/dieselsweetiesweb.py
+++ src/comics/comics/dieselsweetiesweb.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,20 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.dieselsweeties.com/"
     start_date = "2000-01-01"
+    end_date = "2022-04-25"
     rights = "Richard Stevens"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 155
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.dieselsweeties.com/ds-unifeed.xml")
-        for entry in feed.for_date(pub_date):
-            if not hasattr(entry, "summary"):
-                continue
-            url = entry.summary.src('img[src*="/strips666/"]')
-            title = entry.title
-            text = entry.summary.alt('img[src*="/strips666/"]')
-            return CrawlerImage(url, title, text)
+    active = False
