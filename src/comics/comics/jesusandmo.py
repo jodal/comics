6d090bc0fc56cf2fc28a5bd345938fc7770cd85a
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 14:38:19 +0200

    crawlers: Update jesusandmo

--- src/comics/comics/jesusandmo.py
+++ src/comics/comics/jesusandmo.py
@@ -15,29 +15,10 @@ class Crawler(CrawlerBase):
     schedule = "We"
     time_zone = "Europe/London"
 
-    archive_page = None
-
     def crawl(self, pub_date):
-        if not self.archive_page:
-            page_url = "https://www.jesusandmo.net/archives/"
-            self.archive_page = self.parse_page(page_url)
-
-        release = self.archive_page.root.xpath(
-            '//span[(@class="comic-archive-date") and '
-            '(.="%s")]/../span[@class="comic-archive-title"]/a'
-            % pub_date.strftime("%b %d, %Y")
-        )
-        if not release:
-            return
-        release = release[0]
-        link = release.get("href")
-        title = release.text
-        release_page = self.parse_page(link)
-        url = release_page.root.xpath('//div[@id="comic"]/img/@data-src')
-        if not url:
-            return
-        url = url[0]
-        text = release_page.root.xpath('//div[@class="entry"]/p')
-        text = text[0].text if text else None
-
-        return CrawlerImage(url, title, text)
+        feed = self.parse_feed("https://www.jesusandmo.net/comic/feed/")
+        for entry in feed.for_date(pub_date):
+            url = entry.content0.src("img[src*='/uploads/']")
+            if not url:
+                continue
+            return CrawlerImage(url, entry.title)
