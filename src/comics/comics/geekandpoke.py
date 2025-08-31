923f35531e36c6bf1bf29001b25c2d1c16fb35ca
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:24:33 +0200

    crawlers: Mark geekandpoke as inactive

--- src/comics/comics/geekandpoke.py
+++ src/comics/comics/geekandpoke.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,15 +7,4 @@ class ComicData(ComicDataBase):
     url = "http://geek-and-poke.com/"
     start_date = "2006-08-22"
     rights = "Oliver Widder, CC BY-ND 2.0"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 90
-    time_zone = "Europe/Berlin"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/GeekAndPoke")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/static/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
