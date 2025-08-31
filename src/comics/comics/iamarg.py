5e7f8145ed88c66231fdab7bd77656e50b1cb055
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:32:03 +0200

    crawlers: Mark iamarg as inactive

--- src/comics/comics/iamarg.py
+++ src/comics/comics/iamarg.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,23 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://iamarg.com/"
     start_date = "2011-05-08"
+    end_date = "2019-04-01"
     rights = "Andrew Gregoire"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 100
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {"User-Agent": "Mozilla/4.0"}
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://iamarg.com/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/comics-rss/"]')
-            if not url:
-                continue
-            url = url.replace("comics-rss", "comics")
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
