643c8720bed1023307375bfd0ec199ba6dc3b7e1
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:32:50 +0200

    crawlers: Mark nedroid as inactive

--- src/comics/comics/nedroid.py
+++ src/comics/comics/nedroid.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,23 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.nedroid.com/"
     start_date = "2006-04-24"
+    end_date = "2023-08-07"
     rights = "Anthony Clark"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 180
-    time_zone = "America/New_York"
-
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {"User-Agent": "Mozilla/4.0"}
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://nedroid.com/feed/")
-        for entry in feed.for_date(pub_date):
-            if "Comic" not in entry.tags:
-                continue
-            page = self.parse_page(entry.link)
-            url = page.src("#comic img")
-            title = entry.title
-            text = page.title("#comic img")
-            return CrawlerImage(url, title, text)
+    active = False
