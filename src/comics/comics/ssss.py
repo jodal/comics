3aad740e1f3be46b7ab353bc416c8742bf30a78c
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:48:26 +0200

    crawlers: Mark ssss as inactive

--- src/comics/comics/ssss.py
+++ src/comics/comics/ssss.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,17 +6,5 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.sssscomic.com/"
     start_date = "2013-11-01"
+    end_date = "2019-02-14"
     rights = "Minna Sundberg"
-
-
-class Crawler(CrawlerBase):
-    schedule = "Mo,Tu,We,Th,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://sssscomic.com/ssss-feed.xml")
-        for entry in feed.all():
-            page = self.parse_page(entry.link)
-            url = page.src("img.comicnormal")
-            title = entry.title.replace("SSSS page", "Page")
-            return CrawlerImage(url, title)
