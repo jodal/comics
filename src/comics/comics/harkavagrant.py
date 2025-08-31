5cf370c4c07e7214b01ca86752eb0d58e4078421
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:25:41 +0200

    crawlers: Mark harkavagrant as inactive

--- src/comics/comics/harkavagrant.py
+++ src/comics/comics/harkavagrant.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,14 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.harkavagrant.com/"
     start_date = "2008-05-01"
+    end_date = "2018-10-10"
     rights = "Kate Beaton"
-
-
-class Crawler(CrawlerBase):
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        page = self.parse_page("http://www.harkavagrant.com/")
-        url = page.src(".rss-content img")
-        title = page.title(".rss-content img")
-        return CrawlerImage(url, title)
+    active = False
