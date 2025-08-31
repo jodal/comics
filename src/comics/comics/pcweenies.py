249436ec34638587f91f2cf0384b5745c3ad2436
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 14:47:03 +0200

    crawlers: Mark pcweenies as inactive

--- src/comics/comics/pcweenies.py
+++ src/comics/comics/pcweenies.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,19 +7,4 @@ class ComicData(ComicDataBase):
     url = "http://pcweenies.com/"
     start_date = "1998-10-21"
     rights = "Krishna M. Sadasivam"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 14
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://pcweenies.com/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/wp-content/uploads/"]')
-            if not url:
-                continue
-            url = url.replace("-300x120", "")
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
