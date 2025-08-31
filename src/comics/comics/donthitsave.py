15684648fc7fd022ed43d7124fe9edb3d3699dcf
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 14:43:33 +0200

    crawlers: Mark donthitsave as inactive

--- src/comics/comics/donthitsave.py
+++ src/comics/comics/donthitsave.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,17 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "https://donthitsave.com/"
     start_date = "2015-09-18"
+    end_date = "2024-03-05"
     rights = "Jeff Lofvers"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 60
-    schedule = "Tu,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("https://donthitsave.com/donthitsavefeed.xml")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/comicimages/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
