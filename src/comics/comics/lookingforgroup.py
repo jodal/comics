d451d6eeab187c3bd4cc8168d545aeee84d7228c
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 14:39:41 +0200

    crawlers: Mark lookingforgroup as inactive

--- src/comics/comics/lookingforgroup.py
+++ src/comics/comics/lookingforgroup.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,22 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.lfgcomic.com/"
     start_date = "2006-11-06"
+    end_date = "2023-08-31"
     rights = "Ryan Sohmer & Lar deSouza"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 14
-    schedule = "Mo,Th"
-    time_zone = "America/Montreal"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/LookingForGroup")
-        for entry in feed.for_date(pub_date):
-            if not entry.title.isdigit():
-                continue
-            page = self.parse_page(entry.link)
-            if not page.text("title").startswith("Looking For Group"):
-                continue
-            url = page.src("#comic img")
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
