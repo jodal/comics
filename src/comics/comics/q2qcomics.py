09d96e420b6a812e7bc3226da151ccc4f26fafd5
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:35:18 +0200

    crawlers: Mark q2qcomics as inactive

--- src/comics/comics/q2qcomics.py
+++ src/comics/comics/q2qcomics.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,20 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://q2qcomics.com/"
     start_date = "2014-03-08"
+    end_date = "2023-05-10"
     rights = "Steve Younkins"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 14
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://q2qcomics.com/feed/")
-        for entry in feed.for_date(pub_date):
-            if "Comics" not in entry.tags:
-                continue
-            page = self.parse_page(entry.link)
-            url = page.src("img.size-full")
-            title = entry.title.replace("Q2Q Comics ", "")
-            return CrawlerImage(url, title)
+    active = False
