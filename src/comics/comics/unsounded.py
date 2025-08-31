6a7acbbd315626e28c3338ae5f1882c4caa76bab
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:07:03 +0200

    crawlers: Fix unsounded crawler

--- src/comics/comics/unsounded.py
+++ src/comics/comics/unsounded.py
@@ -5,20 +5,23 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = "Unsounded"
     language = "en"
-    url = "http://www.casualvillain.com/Unsounded/"
+    url = "https://unsoundedupdates.tumblr.com/"
     start_date = "2009-10-25"
     rights = "Ashley Cope"
 
 
 class Crawler(CrawlerBase):
     history_capable_days = 50
-    schedule = "Mo,We,Fr"
+    schedule = "Tu,Th,Su"
     time_zone = "America/New_York"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.casualvillain.com/Unsounded/feed/")
+        feed = self.parse_feed("https://unsoundedupdates.tumblr.com/rss")
         for entry in feed.for_date(pub_date):
             page_url = entry.summary.href('a[href*="/comic/"]')
+            if not page_url:
+                continue
+            page_url = page_url.replace("https://href.li/?", "")
             page = self.parse_page(page_url)
             url = page.src("#comic img")
             title = entry.title
