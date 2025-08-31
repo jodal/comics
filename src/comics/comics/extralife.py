e086f1df4b51b5b55282d0c842290ae8e82ed830
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:16:02 +0200

    crawlers: Mark extralife as inactive

--- src/comics/comics/extralife.py
+++ src/comics/comics/extralife.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,19 +7,4 @@ class ComicData(ComicDataBase):
     url = "http://www.myextralife.com/"
     start_date = "2001-06-17"
     rights = "Scott Johnson"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 32
-    schedule = "Mo"
-    time_zone = "America/Denver"
-
-    # Without User-Agent set, the server returns empty responses
-    headers = {"User-Agent": "Mozilla/4.0"}
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.myextralife.com/category/comic/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src('img[src*="/wp-content/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
