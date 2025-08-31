637cad81a62ea8899909dbfcfbbffad3a9521882
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:21:58 +0200

    crawlers: Mark lefthandedtoons as inactive

--- src/comics/comics/lefthandedtoons.py
+++ src/comics/comics/lefthandedtoons.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,20 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.lefthandedtoons.com/"
     start_date = "2007-01-14"
+    end_date = "2018-06-27"
     rights = "Justin & Drew"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 12
-    schedule = "Mo,Tu,We,Th"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/lefthandedtoons/awesome")
-
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/toons/"]')
-            title = entry.title
-
-            if url:
-                return CrawlerImage(url, title)
+    active = False
