647152aaf343759b71ad8e03f90c740d2a785d91
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:29:21 +0200

    crawlers: Mark pidjin as inactive

--- src/comics/comics/pidjin.py
+++ src/comics/comics/pidjin.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,27 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.pidjin.net/"
     start_date = "2006-02-19"
+    end_date = "2021-11-24"
     rights = "Tudor Muscalu & Eugen Erhan"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 90
-    time_zone = "America/Los_Angeles"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/Pidjin")
-        for entry in feed.for_date(pub_date):
-            urls = [
-                url
-                for url in entry.content0.srcs('img[src*="/wp-content/uploads/"]')
-                if (
-                    "ad-RSS" not in url
-                    and "ad5RSS" not in url
-                    and "reddit-txt" not in url
-                    and "rss-box" not in url
-                )
-            ]
-            return [
-                CrawlerImage(url, None, entry.content0.alt('img[src="%s"]' % url))
-                for url in urls
-            ]
+    active = False
