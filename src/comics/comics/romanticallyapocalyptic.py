211d351bc536fda40b5186249926908d44866cfb
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:41:48 +0200

    crawlers: Mark romanticallyapocalyptic as inactive

--- src/comics/comics/romanticallyapocalyptic.py
+++ src/comics/comics/romanticallyapocalyptic.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,16 +6,4 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.romanticallyapocalyptic.com/"
     rights = "Vitaly S. Alexius"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = None
-    schedule = None
-    time_zone = "America/Toronto"
-
-    def crawl(self, pub_date):
-        page = self.parse_page("http://www.romanticallyapocalyptic.com/")
-        urls = page.srcs('img[src*="/art/"]')
-        for url in urls:
-            if "thumb" not in url:
-                return CrawlerImage(url)
+    active = False
