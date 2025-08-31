1c03fe920fe39e1065a828cf49c1f69785539bd8
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:36:34 +0200

    crawlers: Mark thegamercat as inactive

--- src/comics/comics/thegamercat.py
+++ src/comics/comics/thegamercat.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,23 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://thegamercat.com/"
     start_date = "2011-06-10"
+    end_date = "2019-04-15"
     rights = "Celesse"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 180
-    time_zone = "America/Chicago"
-
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {"User-Agent": "Mozilla/4.0"}
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://thegamercat.com/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src("img")
-            if not url:
-                continue
-            url = url.replace("-200x150", "")
-            title = entry.title
-            text = "\n\n".join(entry.content0.texts("p")).strip()
-            return CrawlerImage(url, title, text)
+    active = False
