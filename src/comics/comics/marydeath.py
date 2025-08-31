f81bb4bc7604f0d5b3536ab2c288a605f857acc3
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:28:13 +0200

    crawlers: Mark marydeath as inactive

--- src/comics/comics/marydeath.py
+++ src/comics/comics/marydeath.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,19 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.marydeathcomics.com/"
     start_date = "2013-01-17"
+    end_date = "2019-01-07"
     rights = "Matthew Tarpley, CC BY-NC 3.0"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 365
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.marydeathcomics.com/feed")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/wp-content/uploads/"]')
-            if not url:
-                continue
-            url = url.replace("-150x150", "")
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
