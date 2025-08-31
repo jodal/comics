da4cafcfd8b1a0b34b27bd5cb598a8e0925a11df
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:46:48 +0200

    crawlers: Mark playervsplayer as inactive

--- src/comics/comics/playervsplayer.py
+++ src/comics/comics/playervsplayer.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,22 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://pvponline.com/"
     start_date = "1998-05-04"
+    end_date = "2022-09-16"
     rights = "Scott R. Kurtz"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 14
-    schedule = "Mo,Tu,We,Th,Fr"
-    time_zone = "America/New_York"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://pvponline.com/feed/")
-        for entry in feed.for_date(pub_date):
-            if not entry.title.startswith("Comic:"):
-                continue
-            page = self.parse_page(entry.link)
-            url = page.src('.comic-art img[src*="/comic/"]')
-            if not url:
-                continue
-            title = entry.title.replace("Comic: ", "")
-            return CrawlerImage(url, title)
+    active = False
