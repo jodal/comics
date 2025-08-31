3ae2a08cbb726fb0ba1922604bd500f58f2d8f3a
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:00:12 +0200

    crawlers: Mark doghouse as inactive

--- src/comics/comics/doghouse.py
+++ src/comics/comics/doghouse.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,17 +7,4 @@ class ComicData(ComicDataBase):
     url = "http://www.thedoghousediaries.com/"
     start_date = "2009-01-08"
     rights = "Will, Ray, & Raf"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 30
-    schedule = "Mo,We,Fr"
-    time_zone = "America/Los_Angeles"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/thedoghousediaries/feed")
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src("img.comic-item")
-            title = entry.content0.alt("img.comic-item")
-            text = entry.content0.title("img.comic-item")
-            return CrawlerImage(url, title, text)
+    active = False
