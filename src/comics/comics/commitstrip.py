d843671026785efb2f531eef2c1c5bb464be5335
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 13:59:25 +0200

    crawlers: Mark commitstrip as inactive

--- src/comics/comics/commitstrip.py
+++ src/comics/comics/commitstrip.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,17 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.commitstrip.com/en/"
     start_date = "2012-02-22"
+    end_date = "2022-12-09"
     rights = "Etienne Issartial"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 30
-    schedule = "Mo,Tu,We,Th,Fr"
-    time_zone = "America/Los_Angeles"
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.commitstrip.com/en/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src('img[src*="/uploads/"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+    active = False
