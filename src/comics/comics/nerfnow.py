9b0092383e4f6785ab0898e81ee586833e223f2f
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 14:27:39 +0200

    crawlers: Update nerfnow URLs

--- src/comics/comics/nerfnow.py
+++ src/comics/comics/nerfnow.py
@@ -5,7 +5,7 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = "Nerf NOW!!"
     language = "en"
-    url = "http://www.nerfnow.com/"
+    url = "https://www.nerfnow.com/"
     start_date = "2009-09-02"
     rights = "Josué Pereira"
 
@@ -19,7 +19,7 @@ class Crawler(CrawlerBase):
     headers = {"User-Agent": "Mozilla/4.0"}
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://feeds.feedburner.com/nerfnow/full")
+        feed = self.parse_feed("https://feeds.feedburner.com/nerfnow/full")
         for entry in feed.for_date(pub_date):
             url = entry.content0.src('img[src*="/img/"]')
             if url is None:
