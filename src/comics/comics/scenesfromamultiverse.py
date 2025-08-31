618701043a423f42fb2d87a6c28ffc553cd2624b
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-08-31 02:42:34 +0200

    crawlers: Mark scenesfromamultiverse as inactive

--- src/comics/comics/scenesfromamultiverse.py
+++ src/comics/comics/scenesfromamultiverse.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -7,24 +6,6 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://amultiverse.com/"
     start_date = "2010-06-14"
+    end_date = "2021-08-17"
     rights = "Jonathan Rosenberg"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 40
-    schedule = "Mo,We,Fr"
-    time_zone = "America/New_York"
-
-    # Without User-Agent set, the server returns 403 Forbidden
-    headers = {"User-Agent": "Mozilla/4.0"}
-
-    def crawl(self, pub_date):
-        feed = self.parse_feed("http://amultiverse.com/feed/")
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src('a[rel="bookmark"] img')
-            title = entry.title
-
-            # Text comes in multiple paragraphs: parse out all the text
-            text = "\n\n".join(entry.content0.texts("p")).strip()
-
-            return CrawlerImage(url, title, text)
+    active = False
