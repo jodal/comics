d6fd3cb4a91a2404fa3ae5dd88220f99635b7cc3
Anders Birkenes <datagutten@datagutten.net>
2026-06-20 11:36:40 +0200

    Update crawler for "Evil Inc." after site change

--- src/comics/comics/evilinc.py
+++ src/comics/comics/evilinc.py
@@ -11,20 +11,17 @@ class ComicData(ComicDataBase):
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "2005-05-30"
-    schedule = "Mo,Tu,We,Th,Fr"
+    history_capable_days = 35
+    schedule = "Tu,Th"
     time_zone = "America/New_York"
 
     def crawl(self, pub_date):
-        page_url = "https://www.evil-inc.com/%s/?post_type=comic" % (
-            pub_date.strftime("%Y/%m/%d")
-        )
-
-        page = self.parse_page(page_url)
-
-        url = page.src("img.attachment-large.wp-post-image")
-        if not url:
-            return
-        url = url.replace("?fit=1024%2C1024", "")
-        title = page.text(".post-title")
-        return CrawlerImage(url, title)
+        feed = self.parse_feed("https://www.evil-inc.com/comic/feed/")
+        for entry in feed.for_date(pub_date):
+            title = entry.title
+            page = self.parse_page(entry.link)
+            img = page.root.xpath('//div[@id="unspliced-comic"]/picture/img')
+            if img is None:
+                continue
+            url = img[0].attrib["src"]
+            return CrawlerImage(url, title)
