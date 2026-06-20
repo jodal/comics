8fd446ca01424e606977bd5c7a8384b79bddbd35
Anders Birkenes <datagutten@datagutten.net>
2026-06-20 11:35:01 +0200

    Update crawler for "The Perry Bible Fellowship" after site change

--- src/comics/comics/perrybiblefellowship.py
+++ src/comics/comics/perrybiblefellowship.py
@@ -5,7 +5,7 @@ from comics.core.comic_data import ComicDataBase
 class ComicData(ComicDataBase):
     name = "The Perry Bible Fellowship"
     language = "en"
-    url = "http://www.pbfcomics.com/"
+    url = "https://www.pbfcomics.com/"
     start_date = "2001-01-01"
     rights = "Nicholas Gurewitch"
 
@@ -18,5 +18,8 @@ class Crawler(CrawlerBase):
         feed = self.parse_feed("https://pbfcomics.com/feed/")
         for entry in feed.for_date(pub_date):
             page = self.parse_page(entry.link)
-            urls = [url for url in page.srcs("div#comic img") if url.startswith("http")]
+            urls = [
+                img.attrib["data-src"]
+                for img in page.root.findall('.//div[@id="comic"]/img')
+            ]
             return [CrawlerImage(url, entry.title) for url in urls]
