d6b2cfd05de3375879b2ae16fcb827af60ca69c4
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-07-12 15:42:30 +0200

    chore: Remove Lunch (dn.no) crawler
    Nothing has been published here for a year.

--- src/comics/comics/lunchdn.py
+++ src/comics/comics/lunchdn.py
@@ -1,4 +1,3 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,25 +7,3 @@ class ComicData(ComicDataBase):
     url = "https://www.dn.no/topic/Lunch/"
     active = False
     rights = "Børge Lund"
-
-
-class Crawler(CrawlerBase):
-    history_capable_days = 21  # 3 weeks
-    schedule = "Fr"
-    time_zone = "Europe/Oslo"
-
-    def crawl(self, pub_date):
-        page = self.parse_page(ComicData.url)
-        page_url = page.root.xpath(
-            '//time[@datetime="%s"]/../a/@href' % pub_date.strftime("%Y-%m-%d")
-        )
-        if not page_url:
-            return
-
-        release_page = self.parse_page(page_url[0])
-        image = release_page.root.xpath('//meta[@itemprop="image"]')
-        if not image:
-            return
-        url = image[0].get("content")
-
-        return CrawlerImage(url)
