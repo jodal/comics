ecad89a4263550f46a2a26cb6da9268a531e2ca5
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-08-29 23:47:01 +0200

    chore: Retire garfieldminusgarfield
    The site still answers, but the last post is from 2025-10-12.

--- src/comics/comics/garfieldminusgarfield.py
+++ src/comics/comics/garfieldminusgarfield.py
@@ -1,6 +1,3 @@
-import datetime as dt
-
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage, CrawlerResult
 from comics.core.metadata import MetadataBase
 
 
@@ -8,17 +5,6 @@ class Metadata(MetadataBase):
     name = "Garfield minus Garfield"
     language = "en"
     url = "http://garfieldminusgarfield.tumblr.com/"
+    end_date = "2025-10-12"
     rights = "Travors"
-
-
-class Crawler(CrawlerBase):
-    history_length_days = 30
-    schedule = None
-    time_zone = "Europe/London"
-
-    def crawl(self, pub_date: dt.date) -> CrawlerResult:
-        feed = self.parse_feed("http://garfieldminusgarfield.tumblr.com/rss")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src("img")
-            return CrawlerImage(url)
-        return None
+    active = False
